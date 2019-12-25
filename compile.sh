db03d17e297053d6742e3ecae4cf96b6b4419f1f
Julian D. Otalvaro <juliandavid347@gmail.com>
2019-12-25 15:25:41 -0800

    Julian's server code (not tested)

--- compile.sh
+++ compile.sh
@@ -1,1 +1,12 @@
+library(RSclient)
+rsc <- RSconnect(port = 6311)
+RSshutdown(rsc)
+
+
+remove.packages("Pmetrics")
+rm -rf /Library/Frameworks/R.framework/Versions/3.6/Resources/library/Pmetrics
 Rscript Source/MNmakePmetrics.R
+
+library(Rserve)
+Rserve()
+
