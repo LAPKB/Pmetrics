7518ec31a73ddd87eaf63958bcbd743abd2d1b43
Julian D. Otalvaro <juliandavid347@gmail.com>
2019-12-25 15:52:19 -0800

    tested & working w/alquimia

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
-library(RSclient)
+Rscript -e 'library(RSclient)
 rsc <- RSconnect(port = 6311)
-RSshutdown(rsc)
+RSshutdown(rsc)'
 
-
-remove.packages("Pmetrics")
+Rscript -e 'remove.packages("Pmetrics")'
 rm -rf /Library/Frameworks/R.framework/Versions/3.6/Resources/library/Pmetrics
 Rscript Source/MNmakePmetrics.R
 
+Rscript -e '
 library(Rserve)
-Rserve()
+Rserve()'
 
