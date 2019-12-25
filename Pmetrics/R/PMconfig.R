ad3c661901a0da8fa22ac22f5941dee7b8582d14
Julian D. Otalvaro <juliandavid347@gmail.com>
2019-12-25 14:12:53 -0800

    Neely's Create PMconfig

--- Pmetrics/R/PMconfig.R
+++ Pmetrics/R/PMconfig.R
@@ -1,3 +1,29 @@
 #Returns environment variables used throughout Pmetrics
 
-PMconfig <- function()
+getPMpath <- function() {
+  require(purrr)
+  #return Pmetrics installation folder 
+  unlist(
+    .libPaths() %>%
+    map(~paste(., "Pmetrics", sep = "/")) %>%
+    map(~.[file.exists(.)])
+    )
+}
+
+getBits <- function() {
+  #figure out 32 or 64 bit
+  if (length(grep("64-bit", utils::sessionInfo())) > 0) { return(64) } else { return(32) }
+}
+
+getFixedColNames <- function() {
+  #set current names of fixed columns in data file
+
+  c("id", "evid", "time", "dur", "dose", "addl",
+    "ii", "input", "out", "outeq", "c0", "c1", "c2", "c3")
+}
+
+getFixedColNum <- function() {
+  #set current number of fixed columns in data file
+  length(getFixedColNames())
+}
+
