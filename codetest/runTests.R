9e432caa3291513bff5af3cfa72d02dc62f4340f
Julian D. Otalvaro <juliandavid347@gmail.com>
2019-12-25 14:59:45 -0800

    cleaning

--- codetest/runTests.R
+++
@@ -1,6 +0,0 @@
-results <- test_dir("/Users/Neely/LAPK/PmetricsSource/codetest/Rscript")
-
-
-
-results <- test_dir("/Users/Neely/LAPK/PmetricsSource/codetest/Rscript",filter="^C",reporter="minimal")
-
