75f97e8ae7ae257c35c056c526ced4ae73c195bd
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-01-02 15:37:38 -0800

    Now im receiving and storing the cookie to keep the user logged in as long as the session persists or 24h

--- Pmetrics/R/PMconfig.R
+++ Pmetrics/R/PMconfig.R
@@ -29,3 +29,10 @@ getFixedColNum <- function() {
   return("qoc+7YRUCCK7BmOJrkzNRY6gKzXIGzPPR6IoefaZpOtPkEsKwO48vkCPM18G97Y9")
 }
 
+.setupPMremote <- function() {
+  if (!exists("PMremote")) {
+    PMremote <<- { }
+    PMremote$remoteRuns <<- c()
+  }
+}
+
--- Pmetrics/R/PMremote.R
+++ Pmetrics/R/PMremote.R
@@ -3,7 +3,7 @@ register_user <- function(email, password, password_confirmation, server_address
   library(purrr)
   api_url <- paste0(server_address, "/api")
   r <- POST(
-      paste(api_url, "/user/new", sep = ""),
+      paste0(api_url, "/user/new"),
       body = list(
         email = email,
         password = password,
@@ -19,12 +19,66 @@ login_user <- function(email, password, server_address = "http://localhost:5000"
   library(purrr)
   api_url <- paste0(server_address, "/api")
   r <- POST(
-      paste(api_url, "/session/new", sep = ""),
+      paste0(api_url, "/session/new"),
       body = list(
         email = email,
         password = password),
     encode = "json",
     add_headers(api_key = .getApiKey())
     )
-  content(r)
+  if (r$status == 200) {
+    print("authorized")
+    .setupPMremote()
+    PMremote$session_info <<- cookies(r)$value
+  } else {
+    print("unauthorized")
+  }
+  r
+}
+r <- login_user("juliandavid347@gmail.com", "prueba1234")
+
+.PMremote_run <- function(model, data, server_address) {
+
+  library(httr)
+  library(purrr)
+  api_url <- paste(server_address, "/api", sep = "")
+  model_txt <- readChar(model, file.info(model)$size)
+  data_txt <- readChar(data, file.info(data)$size)
+  r <- POST(
+      paste(api_url, "/analysis/new", sep = ""),
+      body = list(
+        model_txt = model_txt,
+        data_txt = data_txt,
+        name = "prueba"),
+    encode = "json",
+    add_headers(api_key = .getApiKey())
+    )
+  .setupPMremote()
+  PMremote$runs <<- c(PMremote$runs, content(r, "parsed")$id)
+  nRuns <- length(PMremote$runs)
+  sprintf("Remote run #%d started successfuly, You can access this run's id using: PMremote$runs(%d).\n", nRuns, nRuns) %>%
+  cat()
+  PMremote$runs[nRuns] %>%
+  return()
+}
+
+.PMremote_check <- function(rid, server_address) {
+  library(httr)
+  api_url <- paste0(server_address, "/api")
+  r <- GET(paste0(api_url, "/analysis/", rid, "/status"), add_headers(api_key = .getApiKey()))
+  return(content(r, "parsed")$status)
+}
+
+PMremote_outdata <- function(rid, server_address) {
+  library(httr)
+  wd <- getwd()
+  setwd(tempdir())
+  api_url <- paste0(server_address, "/api")
+  r <- GET(paste0(api_url, "/analysis/", rid, "/outdata"), add_headers(api_key = .getApiKey()))
+  fileConn <- file("enc_outdata.txt")
+  writeLines(content(r, "parsed")$outdata, fileConn)
+  close(fileConn)
+  system("base64 --decode -i enc_outdata.txt -o outdata.Rdata")
+  load("outdata.Rdata", .GlobalEnv)
+  setwd(wd)
 }
