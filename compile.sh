0b483058df3913c94e51c5fe5695d563e6b1bb8c
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-01-23 12:27:13 -0800

    compile script

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
-Rscript -e 'library(RSclient)
-rsc <- RSconnect(port = 6311)
-RSshutdown(rsc)'
+#Rscript -e 'library(RSclient)
+#rsc <- RSconnect(port = 6311)
+#RSshutdown(rsc)'
 
 #Rscript -e 'remove.packages("Pmetrics")'
 #rm -rf /Library/Frameworks/R.framework/Versions/3.6/Resources/library/Pmetrics
 Rscript Source/MNmakePmetrics.R
 
-Rscript -e '
-library(Rserve)
-Rserve()'
+#Rscript -e '
+#library(Rserve)
+#Rserve()'
 
 cd
 
