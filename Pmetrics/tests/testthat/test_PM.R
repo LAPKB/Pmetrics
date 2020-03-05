99cb9d6af5b7ae3d6ea8e79f979ce884313ab173
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-03-05 15:03:57 -0800

    fixed an infinite loop when running PMbuild()

--- Pmetrics/tests/testthat/test_PM.R
+++ Pmetrics/tests/testthat/test_PM.R
@@ -4,31 +4,32 @@ library(Pmetrics)
 
 #retain current wd
 currwd <- getwd()
+Sys.setenv(env = "test")
 
 #copy files from testthat to testFolder
-copyNeededFiles <- function(files,testFolder){
+copyNeededFiles <- function(files, testFolder) {
   #if tsstFolder exists, erase it
-  if(file.exists(testFolder)){
-    unlink(testFolder,recursive=T)
+  if (file.exists(testFolder)) {
+    unlink(testFolder, recursive = T)
   }
   #create a new testFolder
   dir.create(testFolder)
   #copy the files
-  file.copy(from=files,to=testFolder)
+  file.copy(from = files, to = testFolder)
 }
 
-test_that("NPAG on rifapentine",{
+test_that("NPAG on rifapentine", {
   testFolder <- "Runs"
-  copyNeededFiles(files=c("model.txt","ex.csv"),testFolder)
+  copyNeededFiles(files = c("model.txt", "ex.csv"), testFolder)
   setwd(testFolder)
-  NPrun(data="ex.csv",include=1:4,cycles=2,intern=T,silent=T)
+  NPrun(data = "ex.csv", include = 1:4, cycles = 2, intern = T, silent = T)
   PMload(1)
-  expect_equal(nrow(final.1$popPoints),4)
-  expect_equal(round(final.1$popPoints$prob[4],7),0.2495361)
-  expect_equal(round(cycle.1$ll[2],5),77.89823)
+  expect_equal(nrow(final.1$popPoints), 4)
+  expect_equal(round(final.1$popPoints$prob[4], 7), 0.2495361)
+  expect_equal(round(cycle.1$ll[2], 5), 77.89823)
   setwd(currwd)
-  unlink(testFolder,recursive=T)
-  
+  unlink(testFolder, recursive = T)
+
 }) #end NPAG on rifapentine test
 
 
