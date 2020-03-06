46ddc82c6facca10c1056f822bf3b26e7bdf6444
mnneely <mneely@usc.edu>
2020-03-05 16:14:29 -0800

    testing export of multiple functions in one file

--- Pmetrics/R/PMoptions.R
+++ Pmetrics/R/PMoptions.R
@@ -12,8 +12,6 @@
 #' @author Michael Neely
 #' @export
 
-
-
 getPMoptions <- function(opt) {
 
   #options file name
@@ -34,4 +32,35 @@ getPMoptions <- function(opt) {
     index <- which(names(PMopts) == opt)
     if (length(index) == 0) { return(NULL) } else { return(PMopts[[index]]) }
   }
+}
+
+#' Set user options for Pmetrics
+#'
+#' This function will set user options for Pmetrics.  
+#'
+#' @title Set Pmetrics User Options
+#' @param sep The field separator character; \dQuote{,} by default, but could be \dQuote{;}
+#' @param dec The decimal separator character; \dQuote{.} by default, but could be \dQuote{,}
+#' @param server_address Specify address of server for remote runs.  Server must be set up separately.
+#' This functionality is coming soon.
+#' @return The user preferences file will be updated.  This will persist from session to session.
+#' @author Michael Neely
+#' @export
+
+setPMoptions <- function(sep, dec, server_address) {
+  #read old values first
+  PMopts <- getPMoptions()
+  #update/add options
+  if (!missing(sep)) PMopts$sep <- sep
+  if (!missing(dec)) PMopts$dec <- dec
+  if (!missing(server_address)) PMopts$server_address <- server_address
+  #set the options
+  options(PMopts)
+  #store the options
+  PMoptionsFile <- paste(system.file("options", package = "Pmetrics"),"PMoptions.json", sep = "/")
+  jsonlite::write_json(PMopts, path = PMoptionsFile, auto_unbox=T)
+}
+
+testFunc <- function(){
+  return("this is a test\n")
 }
