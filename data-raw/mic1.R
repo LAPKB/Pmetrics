4334b95ba2f40fc2f01d25a76bad22412aac2c23
mnneely <mneely@usc.edu>
2020-03-10 14:42:52 -0700

    create mic1 data

--- data-raw/mic1.R
+++ data-raw/mic1.R
@@ -1,6 +1,6 @@
 
 #copied table from http://mic.eucast.org/Eucast2/regShow.jsp?Id=1214 to csv file
-mic <- read.csv("data-raw/mic1.csv")
-names(mic) <- c("mic","n")
+mic1 <- read.csv("data-raw/mic1.csv")
+names(mic1) <- c("mic","n")
 
-usethis::use_data("mic1")
+usethis::use_data(mic1,overwrite=T)
--- data-raw/test.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `test` dataset goes here
-
-usethis::use_data("test")
