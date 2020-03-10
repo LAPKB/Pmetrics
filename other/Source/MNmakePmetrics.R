f7733b672782d7023349ae2a3a77335be920f7d9
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-03-10 14:56:35 -0700

    fixing relatives paths in makePmetrics.R; Package compiled ok

--- other/Source/MNmakePmetrics.R
+++ other/Source/MNmakePmetrics.R
@@ -13,30 +13,30 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
     if (fortranChange == T) {
       #remove old files
       #system("rm ~/LAPK/PmetricsSource/Pmetrics/inst/code/*.f")
-      system(paste(paste("rm ", wd), "/Pmetrics/inst/code/*.f"))
+      system(paste(paste("rm ", wd), "/inst/code/*.f"))
       setpmwd("Source")
 
       #copy source files to inst/code
-      system(paste("cat ./IT2B/prep/*.* > ../Pmetrics/inst/code/ITprep_", ITver, ".f", sep = ""))
-      system(paste("cat ./IT2B/error/*.* > ../Pmetrics/inst/code/ITerr_", ITver, ".f", sep = ""))
-      system(paste("cat ./IT2B/engine/*.* > ../Pmetrics/inst/code/ITeng_", ITver, ".f", sep = ""))
+      system(paste("cat ./IT2B/prep/*.* > ../inst/code/ITprep_", ITver, ".f", sep = ""))
+      system(paste("cat ./IT2B/error/*.* > ../inst/code/ITerr_", ITver, ".f", sep = ""))
+      system(paste("cat ./IT2B/engine/*.* > ../inst/code/ITeng_", ITver, ".f", sep = ""))
 
-      system(paste("cat ./NPAG/prep/*.* > ../Pmetrics/inst/code/NPprep_", NPver, ".f", sep = ""))
-      system(paste("cat ./NPAG/engine/*.* > ../Pmetrics/inst/code/NPeng_", NPver, ".f", sep = ""))
+      system(paste("cat ./NPAG/prep/*.* > ../inst/code/NPprep_", NPver, ".f", sep = ""))
+      system(paste("cat ./NPAG/engine/*.* > ../inst/code/NPeng_", NPver, ".f", sep = ""))
 
-      file.copy(from = paste("./Simulator/engine/MONT", SIMver, ".FOR", sep = ""), to = paste("../Pmetrics/inst/code/SIMeng_", SIMver, ".f", sep = ""), overwrite = T)
+      file.copy(from = paste("./Simulator/engine/MONT", SIMver, ".FOR", sep = ""), to = paste("../inst/code/SIMeng_", SIMver, ".f", sep = ""), overwrite = T)
 
-      system(paste("cat ./DOPT/prep/*.* > ../Pmetrics/inst/code/DOprep_", DOPTver, ".f", sep = ""))
-      system(paste("cat ./DOPT/engine/*.* > ../Pmetrics/inst/code/DOeng_", DOPTver, ".f", sep = ""))
+      system(paste("cat ./DOPT/prep/*.* > ../inst/code/DOprep_", DOPTver, ".f", sep = ""))
+      system(paste("cat ./DOPT/engine/*.* > ../inst/code/DOeng_", DOPTver, ".f", sep = ""))
 
-      system(paste("cat ./MB2CSV/*.* > ../Pmetrics/inst/code/mb2csv_", MBver, ".f", sep = ""))
+      system(paste("cat ./MB2CSV/*.* > ../inst/code/mb2csv_", MBver, ".f", sep = ""))
 
 
       #remove comment lines and clean up
       rmComm <- function(files) {
         for (i in files) {
 
-          system(paste(paste(wd, "Source/win2mac.sa", sep = ""), i))
+          system(paste(paste(wd, "other/Source/win2mac.sa", sep = ""), i))
           file.remove(i)
           file.rename("newfile.txt", i)
           code <- readLines(i)
@@ -52,10 +52,10 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
           writeLines(code, i, sep = "\r\n")
         }
       }
-      setpmwd("Pmetrics/inst/code")
+      setpmwd("../inst/code")
       #setwd("~/LAPK/PmetricsSource/Pmetrics/inst/code")
       rmComm(files = list.files())
-      setpmwd("Pmetrics/inst/config")
+      setpmwd("../inst/config")
       #setwd("~/LAPK/PmetricsSource/Pmetrics/inst/config")
       writeLines("1", "newFort.txt")
 
@@ -63,27 +63,27 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
 
 
     } else {
-      setwd("Pmetrics/inst/config")
+      setwd("inst/config")
       writeLines("0", "newFort.txt")
     }
     #do this for all changes    
     setwd(wd)
     print(getwd())
-    if (file.exists("Pmetrics/src/knn.o")) {
-      file.remove("Pmetrics/src/knn.o")
+    if (file.exists("src/knn.o")) {
+      file.remove("src/knn.o")
     }
-    if (file.exists("Pmetrics/src/Pmetrics.so")) {
-      file.remove("Pmetrics/src/Pmetrics.so")
+    if (file.exists("src/Pmetrics.so")) {
+      file.remove("src/Pmetrics.so")
     }
     load_all("Pmetrics")
     document("Pmetrics")
     if (pdf) {
-      file.remove("./Pmetrics/inst/doc/Pmetrics-manual.pdf")
-      system("R CMD Rd2pdf --output=./Pmetrics/inst/doc/Pmetrics-manual.pdf --no-preview ./Pmetrics/man")
+      file.remove("inst/doc/Pmetrics-manual.pdf")
+      system("R CMD Rd2pdf --output=inst/doc/Pmetrics-manual.pdf --no-preview man/")
     }
 
     #write html changelog file
-    chlog <- readLines("Pmetrics/inst/NEWS.Rd")
+    chlog <- readLines("inst/NEWS.Rd")
     delLines <- which(chlog == "")
     chlog <- chlog[-delLines]
     chlog <- chlog[-1]
@@ -106,7 +106,7 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
       file.copy(from = c("../src/model.txt", "../src/ex.csv"), to = getwd(), overwrite = T)
       NPrun(data = "ex.csv", cycles = 100, run = 1, overwrite = T, intern = T)
       PMload(1)
-      save(NPdata.1, final.1, cycle.1, op.1, cov.1, pop.1, post.1, mdata.1, file = paste(wd, "Pmetrics/data/PMex1.rda"))
+      save(NPdata.1, final.1, cycle.1, op.1, cov.1, pop.1, post.1, mdata.1, file = paste(wd, "data/PMex1.rda"))
     }
     if (buildData$it2b) {
       #IT2B
@@ -116,13 +116,13 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
       ITrun(data = "ex.csv", cycles = 100, run = 1, overwrite = T, intern = T)
       PMload(1)
       # save(ITdata.1,final.1,cycle.1,op.1,cov.1,mdata.1,file="~/LAPK/PmetricsSource/Pmetrics/data/PMex2.rda")
-      save(ITdata.1, final.1, cycle.1, op.1, cov.1, mdata.1, file = paste(wd, "Pmetrics/data/PMex2.rda"))
+      save(ITdata.1, final.1, cycle.1, op.1, cov.1, mdata.1, file = paste(wd, "data/PMex2.rda"))
     }
     if (buildData$baddata) {
       setpmwd("Test")
       #setwd("~/LAPK/PmetricsSource/Test")
       badData <- PMreadMatrix("src/ex_bad.csv")
-      save(badData, file = paste(wd, "Pmetrics/data/PMex3.rda"))
+      save(badData, file = paste(wd, "data/PMex3.rda"))
       # save(badData,file="~/LAPK/PmetricsSource/Pmetrics/data/PMex3.rda")
     }
     setwd(wd)
@@ -142,16 +142,16 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
 
     #tar.gz
     tools::write_PACKAGES(type = "source")
-    file.remove(Sys.glob("Repos/src/contrib/Pmetrics*.tar.gz"))
-    file.copy(from = Sys.glob("Pmetrics*.tar.gz"), to = "Repos/src/contrib")
+    file.remove(Sys.glob("other/Repos/src/contrib/Pmetrics*.tar.gz"))
+    file.copy(from = Sys.glob("Pmetrics*.tar.gz"), to = "other/Repos/src/contrib")
     file.copy(from = Sys.glob("Pmetrics*.tar.gz"), to = "Archived")
-    file.copy(from = "PACKAGES", to = "Repos/src/contrib", overwrite = T)
+    file.copy(from = "PACKAGES", to = "other/Repos/src/contrib", overwrite = T)
     file.remove(Sys.glob("Pmetrics*.tar.gz"))
 
 
     #tgz
     tools::write_PACKAGES(type = "mac.binary")
-    macBinDir <- paste("Repos/bin/macosx/contrib/", Rvers, sep = "")
+    macBinDir <- paste("other/Repos/bin/macosx/contrib/", Rvers, sep = "")
     if (!file.exists(macBinDir)) {
       dir.create(macBinDir)
     }
@@ -161,7 +161,7 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
     file.copy(from = "PACKAGES", to = macBinDir, overwrite = T)
 
 
-    mavBinDir <- paste("Repos/bin/macosx/mavericks/contrib/", Rvers, sep = "")
+    mavBinDir <- paste("other/Repos/bin/macosx/mavericks/contrib/", Rvers, sep = "")
     if (!file.exists(mavBinDir)) {
       dir.create(mavBinDir)
     }
@@ -188,7 +188,7 @@ makePmetrics <- function(fortranChange = F, build = T, pdf = F, check = F, build
     Rvers <- paste(version$major, substr(version$minor, 1, 1), sep = ".")
     #zip
     tools::write_PACKAGES(type = "win.binary")
-    winBinDir <- paste(wd, paste("Repos/bin/windows/contrib/", Rvers, sep = ""), sep = "")
+    winBinDir <- paste(wd, paste("other/Repos/bin/windows/contrib/", Rvers, sep = ""), sep = "")
     print(winBinDir)
     if (!file.exists(winBinDir)) {
       dir.create(winBinDir)
