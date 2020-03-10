ea4ef3c13a1447fde6cd07470ad6089cd11d2447
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-03-10 14:56:35 -0700

    fixing relatives paths in makePmetrics.R; Package compiled ok

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 
 #Rscript -e 'remove.packages("Pmetrics")'
 #rm -rf /Library/Frameworks/R.framework/Versions/3.6/Resources/library/Pmetrics
-Rscript Source/MNmakePmetrics.R
+Rscript other/Source/MNmakePmetrics.R
 
 #Rscript -e '
 #library(Rserve)
