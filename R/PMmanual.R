5428eadf6a13375a3b59638e26d4c4aeb1edfae6
mnneely <mneely@usc.edu>
2020-03-24 12:04:59 -0700

    manual

--- R/PMmanual.R
+++ R/PMmanual.R
@@ -8,12 +8,12 @@
 PMmanual <- function() {
   OS <- getOS()
   if (OS == 1 | OS == 3) {
-    system(paste("open ", normalizePath(getPMpath(), winslash = "/"), "/Pmetrics/doc/Pmetrics-manual.pdf", sep = ""))
-    system(paste("open ", normalizePath(getPMpath(), winslash = "/"), "/Pmetrics/doc/PM_User_manual.pdf", sep = ""))
+    system(paste("open ", normalizePath(getPMpath(), winslash = "/"), "/doc/Pmetrics-manual.pdf", sep = ""))
+    system(paste("open ", normalizePath(getPMpath(), winslash = "/"), "/doc/PM_User_manual.pdf", sep = ""))
   }
   if (OS == 2) {
-    shell(paste("start acrord32.exe ", shQuote(normalizePath(paste(getPMpath(), "/Pmetrics/doc/PM_User_manual.pdf", sep = ""), winslash = "\\")), sep = ""))
-    shell(paste("start acrord32.exe ", shQuote(normalizePath(paste(getPMpath(), "/Pmetrics/doc/Pmetrics-manual.pdf", sep = ""), winslash = "\\")), sep = ""))
+    shell(paste("start acrord32.exe ", shQuote(normalizePath(paste(getPMpath(), "/doc/PM_User_manual.pdf", sep = ""), winslash = "\\")), sep = ""))
+    shell(paste("start acrord32.exe ", shQuote(normalizePath(paste(getPMpath(), "/doc/Pmetrics-manual.pdf", sep = ""), winslash = "\\")), sep = ""))
   }
 }
 
