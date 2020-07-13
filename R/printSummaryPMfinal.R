6cb5ee7467e93a7bc59c4dcaee19f8defbe9fb5d
mnneely <mneely@usc.edu>
2020-07-13 16:35:20 -0700

    Fixed print bug with tidyverse object.

--- R/printSummaryPMfinal.R
+++ R/printSummaryPMfinal.R
@@ -15,17 +15,18 @@
 
 print.summary.PMfinal <- function(x, digits=max(3,getOption("digits")-3),...){
   
+  x <- data.frame(x) #convert from tibble
   cat(paste("\nWeighted Medians (", 100*(max(x$percentile)-min(x$percentile)),
             "% credibility interval)\n",sep=""))  
   for(i in 1:(ncol(x)-2)){
-    cat(paste(colnames(x[,i]),": ",round(x[2,i],digits)," (",round(x[1,i],digits)," - ",round(x[3,i],digits),")\n",sep=""))
+    cat(paste(colnames(x[i]),": ",round(x[2,i],digits)," (",round(x[1,i],digits)," - ",round(x[3,i],digits),")\n",sep=""))
   }
   
   cat(paste("\nMedian Absolute Weighed Differences (dispersion measure) (", 100*(max(x$percentile)-min(x$percentile)),
             "% credibility interval)\n",sep=""))
   
   for(i in 1:(ncol(x)-2)){
-    cat(paste(colnames(x[,i]),": ",round(x[5,i],digits)," (",round(x[4,i],digits)," - ",round(x[6,i],digits),")\n",sep=""))
+    cat(paste(colnames(x[i]),": ",round(x[5,i],digits)," (",round(x[4,i],digits)," - ",round(x[6,i],digits),")\n",sep=""))
   }
   
  
