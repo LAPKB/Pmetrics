f63a8232135d7b8e93057843918624bef98e3ea9
Michael Neely <mneely@usc.edu>
2022-03-10 16:44:04 -0800

    not exporting and unsure why

--- R/PM_tutorial.R
+++ R/PM_tutorial.R
@@ -1,5 +1,6 @@
 
 #' @export
+#' 
 PM_tutorial <- function(wd = getwd()){
   cat("This copies to your current working directory an \"Examples\" folder,
   which contains all the files and folders you need to work through an
