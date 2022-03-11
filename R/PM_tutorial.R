8aa0fd284f55b47e827703454828b02b6fe350d5
Michael Neely <mneely@usc.edu>
2022-03-10 16:12:08 -0800

    Added print function to PM_model named first field in $out as $value Added PM_tutorial and Examples folder to inst

---
+++ R/PM_tutorial.R
@@ -0,0 +1,23 @@
+
+
+PM_tutorial <- function(wd = getwd()){
+  cat("This copies to your current working directory an \"Examples\" folder,
+  which contains all the files and folders you need to work through an
+  introductory tutorial.\n")
+  cat("\nYour current working directory:",getwd(),"\n")
+  ans <- readline("Press \"q\" to quit, \"return\" to use your current working directory, or enter a new one. ")
+  if(tolower(substr(ans,1,1))=="q"){
+    cat("No files copied.\n")
+    return(invisible())
+  } else if(ans!=""){
+    if(dir.exists(ans)){
+      stop(paste0(ans, " already exists.\n"))
+    } else {dir.create(ans)
+      wd <- ans}
+  }
+  
+  file.copy(system.file("Examples", package="Pmetrics"), wd, recursive = T)
+  script <- readr::read_lines(paste0(wd,"/Examples/Rscript/examples.R")) %>%
+    stringr::str_replace_all("##WD##",wd)
+  readr::write_lines(script,paste0(wd,"/Examples/Rscript/examples.R"))
+}
