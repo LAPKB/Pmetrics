354d5dcf0f31b8d81d88ee8b551763f85c5e2886
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-03-25 19:27:23 -0500

    remove the 0s message from PMreadMatrix

--- R/PMreadMatrix.R
+++ R/PMreadMatrix.R
@@ -71,7 +71,7 @@ PMreadMatrix <- function(file,skip=1,sep=getPMoptions("sep"),dec=getPMoptions("d
   
   data <- readr::read_delim(file, delim = sep, col_names = T, na=".", 
                             locale = readr::locale(decimal_mark = dec, date_format = date_format, time_format = time_format),
-                            skip = skip, show_col_types = F)
+                            skip = skip, show_col_types = F, progress = F)
   names(data)[1] <- "id"
   names(data) <- tolower(names(data))
 
