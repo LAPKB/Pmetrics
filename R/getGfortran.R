171f05bf2deb90e68161738832adba608e3d4aa1
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-03-30 12:07:09 -0700

    trying to fix brew for Apple M1

--- R/getGfortran.R
+++ R/getGfortran.R
@@ -10,7 +10,6 @@ update_gfortran <- function() {
   OS <- getOS()
   if (OS == 1 || OS == 2) {
     .installOrUpdateGCC(OS)
-  
   } else {
     cat("This functionality is currently only working in MacOS, if you are using another OS\nremove your current gfortran installation and run PMbuild()")
   }
@@ -20,7 +19,7 @@ update_gfortran <- function() {
   OS <- getOS()
   if (OS == 1) {
     cat("Checking for Brew \n")
-    if(!.installOrUpdateBrew()){
+    if (!.installOrUpdateBrew()) {
       return(F)
     }
     .installOrUpdateGCC(OS)
@@ -33,12 +32,11 @@ update_gfortran <- function() {
       return(TRUE)
     }
   } else if (OS == 2) {
-    return = system2("C:/Windows/System32/WindowsPowerShell/v1.0/powershell", args = c("-ExecutionPolicy","ByPass","-file", paste(system.file("", package = "Pmetrics"), "win/exec.ps1", sep = "/")), wait = T)
+    return <- system2("C:/Windows/System32/WindowsPowerShell/v1.0/powershell", args = c("-ExecutionPolicy", "ByPass", "-file", paste(system.file("", package = "Pmetrics"), "win/exec.ps1", sep = "/")), wait = T)
     cat("Background installation of gfortran started.\n")
     readline(prompt = "After installation is complete, press RETURN to continue.\n")
     return(TRUE)
-  }
-  else if (OS == 3) {
+  } else if (OS == 3) {
     cat("Pmetrics is checking for build essentials...\n")
     if (system("which gcc") != 0) {
       cat("Pmetrics did not find build essentials - Installing... \n")
@@ -57,47 +55,46 @@ update_gfortran <- function() {
   }
 }
 
-.installOrUpdateBrew <- function(){
-  if (system("which -s brew") != 0) {
-    #Brew is not installed
+.installOrUpdateBrew <- function() {
+  if (system("which -s /opt/homebrew/bin/brew") != 0) {
+    # Brew is not installed
     cat("Pmetrics did not find the Homebrew Package Manager and will attempt to download and install it.\n")
     input <- tolower(readline("Do you wish to proceed? (Y/N)"))
-    if (substr(input,1,1) == "n") {
+    if (substr(input, 1, 1) == "n") {
       return(FALSE)
     }
-    script = paste(system.file("", package = "Pmetrics"), "mac/install_homebrew.sh", sep = "/")
+    script <- paste(system.file("", package = "Pmetrics"), "mac/install_homebrew.sh", sep = "/")
     system(paste("chmod +x ", script))
     system(paste0("open -a Terminal.app '", script, "'"))
-    #system("ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
+    # system("ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
     cat("A new terminal will open and ask for permission to install.  Enter your computer password.\n")
-    input <- readline(prompt =  "Press any key when Homebrew installation is complete.\n")
+    input <- readline(prompt = "Press any key when Homebrew installation is complete.\n")
   } else {
-    #brew is installed
+    # brew is installed
     cat("Pmetrics found Homebrew found and will ensure latest version is installed.\n")
-    system("brew update")
+    system(" update")
   }
   return(T)
 }
 
-.installOrUpdateGCC <- function(OS){
+.installOrUpdateGCC <- function(OS) {
   if (OS == 1) {
-     if (system("brew ls --versions gcc") != 0) {
+    if (system("/opt/homebrew/bin/brew ls --versions gcc") != 0) {
       cat("Pmetrics did not find GCC and will install it.\n")
-      system("brew install gcc")
+      system("/opt/homebrew/bin/brew install gcc")
     } else {
       cat("Pmetrics found GCC.\n")
-      if(system("brew outdated | grep gcc")==0){
-        #there is a new version of GCC
+      if (system("/opt/homebrew/bin/brew outdated | grep gcc") == 0) {
+        # there is a new version of GCC
         cat("Pmetrics found a newer version of GCC and will update it.\n")
-        system("brew upgrade gcc")
+        system("/opt/homebrew/bin/brew upgrade gcc")
       }
     }
   } else if (OS == 2) {
-     if(system2("C:/Windows/System32/WindowsPowerShell/v1.0/powershell", "choco outdated | grep mingw") ==0){
-       #there is a new version of mingw
-        cat("Pmetrics found a newer version of GCC and will update it.\n")
-        system("choco upgrade mingw -y")
-     }
+    if (system2("C:/Windows/System32/WindowsPowerShell/v1.0/powershell", "choco outdated | grep mingw") == 0) {
+      # there is a new version of mingw
+      cat("Pmetrics found a newer version of GCC and will update it.\n")
+      system("choco upgrade mingw -y")
+    }
   }
-  
 }
