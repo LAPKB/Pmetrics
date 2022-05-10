9c12dc2026e4fd853631bfa263d8fbe6a2b0ec2f
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-05-09 21:03:37 -0500

    autoyes for PMbuild()

--- R/PMbuild.R
+++ R/PMbuild.R
@@ -7,14 +7,12 @@
 
 
 
-PMbuild <- function(skipRegistration = F) {
-
-  if (.check_and_install_gfortran(skipRegistration)) {
-
+PMbuild <- function(skipRegistration = F, autoyes = F) {
+  if (.check_and_install_gfortran(skipRegistration, autoyes)) {
     currwd <- getwd()
     OS <- getOS()
 
-    #load necessary packages
+    # load necessary packages
     # packages <- packageDescription("Pmetrics")$Suggests
     # packages <- gsub("\n", "", packages)
     # packages <- unlist(strsplit(packages, ","))
@@ -28,35 +26,44 @@ PMbuild <- function(skipRegistration = F) {
     # }
 
     compiler <- PMFortranConfig()
-    #try again just in case redefined
+    # try again just in case redefined
     compiler <- PMFortranConfig()
-    #check if parallel is possible
+    # check if parallel is possible
     if (length(compiler) == 2 & getBits() == "64") {
       parallel <- T
-    } else { parallel <- F }
+    } else {
+      parallel <- F
+    }
     sourcedir <- system.file("code", package = "Pmetrics")
     destdir <- paste(system.file("", package = "Pmetrics"), "compiledFortran", sep = "/")
-    #remove old files if present
+    # remove old files if present
     oldfiles <- c(Sys.glob(paste(destdir, "*.o", sep = "/")), Sys.glob(paste(destdir, "*.exe", sep = "/")))
 
-    if (length(oldfiles) > 0) { file.remove(oldfiles) }
-    #compile new files
+    if (length(oldfiles) > 0) {
+      file.remove(oldfiles)
+    }
+    # compile new files
     setwd(sourcedir)
     if (!file.exists(destdir)) dir.create(destdir, showWarnings = F)
     PMfiles <- data.frame(filename = as.character(c("NPprep", "NPeng", "ITprep", "ITeng", "ITerr", "SIMeng", "DOprep", "DOeng", "mb2csv")))
-    PMfiles$path <- sapply(PMfiles$filename,
-   function(x) shQuote(
-    list.files(
-      getwd(), pattern = as.character(paste(x, "_[[:digit:]]+\\.f", sep = ""))
+    PMfiles$path <- sapply(
+      PMfiles$filename,
+      function(x) {
+        shQuote(
+          list.files(
+            getwd(),
+            pattern = as.character(paste(x, "_[[:digit:]]+\\.f", sep = ""))
+          )
+        )
+      }
     )
-  ))
 
 
     for (i in 1:nrow(PMfiles)) {
       cat(paste("\nCompiling ", i, " of ", nrow(PMfiles), ": ", PMfiles$filename[i], "...", sep = ""))
       flush.console()
       if (PMfiles$filename[i] %in% c("DOprep", "mb2csv")) {
-        #list of compiled and linked files
+        # list of compiled and linked files
         serialCommand <- sub("<exec>", paste(PMfiles$filename[i], ".exe", sep = ""), compiler[1])
         serialCommand <- sub("<files>", PMfiles$path[i], serialCommand)
       } else {
@@ -65,9 +72,11 @@ PMbuild <- function(skipRegistration = F) {
       }
       serialFortstatus <- suppressWarnings(system(serialCommand, intern = T, ignore.stderr = F))
       if (!is.null(attr(serialFortstatus, "status"))) {
-        unlink(switch(OS, "~/.config/Pmetrics",
-                    paste(Sys.getenv("APPDATA"), "\\Pmetrics", sep = ""),
-                    "~/.config/Pmetrics"), recursive = T)
+        unlink(switch(OS,
+          "~/.config/Pmetrics",
+          paste(Sys.getenv("APPDATA"), "\\Pmetrics", sep = ""),
+          "~/.config/Pmetrics"
+        ), recursive = T)
         stop(paste("\nThere was an error compiling ", PMfiles$filename[i], ".\nDid you select the right fortran compiler?  If yes, try reinstalling fortran.\nFor gfortran, log into www.lapk.org and access system-specific tips on the Pmetrics installation page (step 5).\n", sep = ""))
       }
       if (i == 2 & parallel) {
@@ -76,13 +85,14 @@ PMbuild <- function(skipRegistration = F) {
         parallelCommand <- sub("<files>", PMfiles$path[i], parallelCommand)
         parallelFortstatus <- suppressWarnings(system(parallelCommand, intern = T, ignore.stderr = F))
         if (!is.null(attr(parallelFortstatus, "status"))) {
-          unlink(switch(OS, "~/.config/Pmetrics",
-                      paste(Sys.getenv("APPDATA"), "\\Pmetrics", sep = ""),
-                      "~/.config/Pmetrics"), recursive = T)
+          unlink(switch(OS,
+            "~/.config/Pmetrics",
+            paste(Sys.getenv("APPDATA"), "\\Pmetrics", sep = ""),
+            "~/.config/Pmetrics"
+          ), recursive = T)
           stop(paste("\nThere was an error compiling ", PMfiles$filename[i], ".\nDid you select the right fortran compiler?  If yes, try reinstalling fortran.\nFor gfortran, log into www.lapk.org and access system-specific tips on the Pmetrics installation page (step 5).\n", sep = ""))
         }
       }
-
     }
 
 
@@ -91,29 +101,34 @@ PMbuild <- function(skipRegistration = F) {
     invisible(file.copy(from = Sys.glob(c("*.o", "*.exe")), to = destdir))
     invisible(file.remove(Sys.glob(c("*.o", "*.exe"))))
     fort <- paste(system.file("config", package = "Pmetrics"), "newFort.txt", sep = "/")
-    writeLines("0", fort) #reset to zero
+    writeLines("0", fort) # reset to zero
     setwd(currwd)
   }
 }
 
-.check_and_install_gfortran <- function(skipRegistration) {
-  #restore user defaults - deprecated
-  #if(length(system.file(package="Defaults"))==1){PMreadDefaults()}
+.check_and_install_gfortran <- function(skipRegistration, autoyes) {
+  # restore user defaults - deprecated
+  # if(length(system.file(package="Defaults"))==1){PMreadDefaults()}
   sch_str <- c("which -s gfortran", "where gfortran", "which -s gfortran")
   OS <- getOS()
-  env = Sys.getenv("env")
+  env <- Sys.getenv("env")
   if (env != "Development") {
     if (!binaries.installed()) {
       cat("Pmetrics cannot find required compiled binary files.\n")
       if (system(sch_str[OS]) != 0) {
         cat("Pmetrics cannot detect gfortran and will attempt to download and install all components.\n")
-        input <- tolower(readline(prompt = "Do you agree? (Y/N)"))
+
+        input <- if (autoyes) {
+          "y"
+        } else {
+          tolower(readline(prompt = "Do you agree? (Y/N)"))
+        }
         if (substr(input, 1, 1) == "y") {
-          if (.installOrUpdateGfortran()) {
+          if (.installOrUpdateGfortran(autoyes)) {
             cat("Pmetrics has installed gfortran and will now compile required binary files.\n")
             cat("Pmetrics has anonymously registered your installation of this version.\nLAPKB does not collect or store any personal or identifying information.")
             cat("If the registration time outs, please run PMbuild(skipRegistration=T) ")
-            if(skipRegistration == F){
+            if (skipRegistration == F) {
               .PMremote_registerNewInstallation()
             }
             return(T)
@@ -129,7 +144,7 @@ PMbuild <- function(skipRegistration = F) {
         cat("Pmetrics has detected gfortran and will compile required binary files.\n")
         cat("Pmetrics has anonymously registered your installation of this version.\nLAPKB does not collect or store any personal or identifying information.")
         cat("If the registration time outs, please run PMbuild(skipRegistration=T) ")
-        if(skipRegistration == F){
+        if (skipRegistration == F) {
           .PMremote_registerNewInstallation()
         }
         return(T)
@@ -138,8 +153,7 @@ PMbuild <- function(skipRegistration = F) {
       cat("Pmetrics has found required compiled binary files.\n")
       return(F)
     }
-  }
-  else {
+  } else {
     print("You are inside the development folder, skipping gfortran installation")
     return(F)
   }
