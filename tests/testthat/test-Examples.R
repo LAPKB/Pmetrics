e2448230fb0cc87471dbe09c6cab14f2dad65070
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-07-06 13:52:22 -0500

    The tests are now going through full runs

--- tests/testthat/test-Examples.R
+++ tests/testthat/test-Examples.R
@@ -108,3 +108,55 @@ expect_equal(mod1$model_list$pri$Ka$min, 0.001)
 expect_equal(mod1$model_list$pri$Ka$mean, 2.5)
 expect_equal(mod1$model_list$pri$Ka$sd, 0.833)
 })
+
+
+test_that("Fit object creation",{
+  mod1 <- PM_model$new(list(
+    pri = list(
+      Ka = range(0.1, 0.9),
+      Ke = range(0.001, 0.1),
+      V = range(30, 120),
+      Tlag1 = range(0, 4)
+    ),
+    cov = list("WT", "AFRICA", "AGE", "GENDER", "HEIGHT"),
+    lag = list("Tlag(1) = Tlag1"),
+    out = list(
+      Y1 = list(
+        value = "X(2)/V",
+        err = list(
+          model = proportional(5),
+          assay = c(0.02, 0.05, -0.0002, 0)
+        )
+      )
+    )
+  ))
+  exFit <- PM_fit$new(model = mod1, data = "ex.csv")
+  expect_output(exFit$check(),"Excellent - there were no errors found in your model file.")
+  expect_output(exFit$check(),"No data errors found.")
+})
+
+test_that("Basic model fitting", {
+  mod1 <- PM_model$new(list(
+    pri = list(
+      Ka = range(0.1, 0.9),
+      Ke = range(0.001, 0.1),
+      V = range(30, 120),
+      Tlag1 = range(0, 4)
+    ),
+    cov = list("WT", "AFRICA", "AGE", "GENDER", "HEIGHT"),
+    lag = list("Tlag(1) = Tlag1"),
+    out = list(
+      Y1 = list(
+        value = "X(2)/V",
+        err = list(
+          model = proportional(5),
+          assay = c(0.02, 0.05, -0.0002, 0)
+        )
+      )
+    )
+  ))
+  exFit <- PM_fit$new(model = mod1, data = "ex.csv")
+  
+  expect_output(exFit$run(intern = T),"The run did not converge before the last cycle.")
+
+})
