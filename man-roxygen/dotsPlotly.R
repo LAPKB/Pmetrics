e212a4ec617e8e1588b75f9bc822fe4a915d4718
Michael Neely <mneely@usc.edu>
2022-08-18 13:13:08 -0700

    Updating documentation with templates for shared arguments. Made plot.PMvalid more tidy.

---
+++ man-roxygen/dotsPlotly.R
@@ -0,0 +1,3 @@
+#' @param \dots Other parameters which can be passed to control axis formatting. Specify as lists
+#' named either xaxis or yaxis. See `plotly::schema()` layout > layoutAttributes > xaxis/yaxis for options.
+#' Example: `xaxis = list(title = list(text = "Time (h)", font = list(family = "Arial", size = 14, color = "black")))`
---
+++ man-roxygen/xlim.R
@@ -0,0 +1,7 @@
+#' @param xlim Limits of the x axis as a vector.  
+#' This argument maps to the  
+#' the xaxis range in the layout object in plotly. 
+#' Use the plotly `plotly::schema()` command in the console and navigate
+#' to layout > layoutAttributes > xaxis > range.
+#' Example: `xlim = c(0,1)`
+
