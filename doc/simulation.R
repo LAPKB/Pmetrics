cbffa7fff37b70def7791876e0c743a2ef571030
Michael Neely <mneely@usc.edu>
2022-09-27 18:36:11 +0100

    more documentation updates

---
+++ doc/simulation.R
@@ -0,0 +1,12 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, echo=F, message=F-------------------------------------------------
+library(Pmetrics)
+
+## ----fig.show = "hold", out.width = "45%", echo = F---------------------------
+knitr::include_graphics(c("Images/unibv40.png","Images/multi-bv40.png"))
+
