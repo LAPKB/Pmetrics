52b22787f6403e5e67fb494e4bee1a0e39a88e3f
Michael Neely <mneely@usc.edu>
2022-11-04 12:29:38 -0700

    Made variable date/time format handling more robust

--- R/PM_data.R
+++ R/PM_data.R
@@ -5,12 +5,22 @@
 #' to process the data
 #' 
 #' @details
-#' PM_data objects are passed to [PM_fit] objects to initiate a
+#' *PM_data* objects are passed to [PM_fit] objects to initiate a
 #' population analysis. The object is created by reading a delimited file in 
 #' the current working directory. The data will be transformed into the standard
 #' format which is the same for all engines, with a report of any assumptions
 #' that were necessary to standardize the data. [PMcheck] is called
-#' on the standard data to evaluate for errors. There are a number of methods
+#' on the standard data to evaluate for errors. If dates and times are converted
+#' to relative decimal times in the standard data, automatic detection of the correct
+#' format will be attempted using [lubridate::parse_date_time()]. In the case of failure
+#' due to an unusual format, use the 'dt' argument to specify the correct format in your
+#' data. In the case of successful automatic detection, the format used will be
+#' included in the standardization report generated upon creation of a new *PM_data*
+#' object. Check carefully to make sure the correct format was chosen. Note that if
+#' your clock times did not include seconds, they were appended as ":00" to the end 
+#' of each time and will appear that way in the copy of the original data.
+#' 
+#' There are a number of methods
 #' defined for a PM_data object, including to write the standard data back 
 #' to a file for future use, to summarize and to plot the object, and to
 #' conduct a non-compartmental analysis on the raw data using 
@@ -32,19 +42,22 @@ PM_data <- R6::R6Class("PM_data",
     #' @param data A quoted name of a file with full path if not
     #' in the working directory, or an unquoted name of a data frame
     #' in the current R environment.
-    #' @param dt Date time format as a character vector whose
-    #' first element is date format and second is time. Default is 
-    #' `c("Ymd", "HS")`. Use the following abbreviations:
+    #' @param dt Pmetrics will try a variety of date/time formats. If all 16 of
+    #' them fail, use this parameter to specify the correct format as a 
+    #' character vector whose
+    #' first element is date format and second is time. Use the following abbreviations:
     #' * Y = 4 digit year
     #' * y = 2 digit year
     #' * m = decimal month (1, 2, ..., 12)
     #' * d = decimal day (1, 2, ..., 31)
     #' * H = hours (0-23)
     #' * M = minutes (0-59)
-    #' Other formats are possible. See [strptime] for these,
-    #' omitting the % before each abbreviation.
+    #' Example: `format = c("myd", "mh")`. Not one of the tried combinations!
+    #' Always check to make sure that dates/times were parsed correctly and the 
+    #' relative times in the `PM_data$standard_data` field look correct.
+    #' Other date/time formats are possible. See [lubridate::parse_date_time()] for these.
     #' @param quiet Quietly validate. Default is `FALSE`.
-    initialize = function(data, dt = c("Ymd", "HS"), quiet = F) {
+    initialize = function(data, dt = NULL, quiet = F) {
       self$data <- if (is.character(data)) {
         PMreadMatrix(data, quiet = T)
       } else {
@@ -145,7 +158,7 @@ PM_data <- R6::R6Class("PM_data",
         relTime <- PMmatrixRelTime(dataObj, format = dt)
         dataObj$time <- relTime$relTime
         dataObj <- dataObj %>% select(-date)
-        msg <- c(msg, "Dates and clock times converted to relative decimal times.\n")
+        msg <- c(msg, paste0("Dates and clock times converted to relative decimal times using ",attr(relTime,"dt_format"),".\n"))
       }
 
       if (!"dur" %in% dataNames) {
