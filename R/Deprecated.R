782144849f8125bfa7f834ab5b379b016f6cb0a9
Julian D. Otálvaro <juliandavid347@gmail.com>
2022-11-23 12:40:17 +0100

    Removed PMFortranConfig, moved the definition of the compulation statements to PMoptions and added a new argument to PMbuild to allow rebuilds

--- R/Deprecated.R
+++ R/Deprecated.R
@@ -1,65 +1,68 @@
 #' The following functions are deprecated in Pmetrics.
-#' 
-#' 
+#'
+#'
 #' @title Deprecated functions.
-#' @aliases PMcheckMatrix, PMfixMatrix, NPload, ITload, NPreport, ITreport, 
+#' @aliases PMFortranConfig, PMcheckMatrix, PMfixMatrix, NPload, ITload, NPreport, ITreport,
 #' PMdiag, PMreadDefaults, PMwriteDefaults, makeNPDE, PMupdate
 #' @author Michael Neely
 #' @export
 
+#' @export
+PMFortranConfig <- function(...) {
+  cat("\nPMFortranConfig is deprecated.  Use setPMoptions instead to change the compilation arguments.\n")
+}
 
-
-PMcheckMatrix <- function(...){
+#' @export
+PMcheckMatrix <- function(...) {
   cat("\nPMcheckMatrix is deprecated.  Use PMcheck() instead to check data and model files.\n")
 }
 
 #' @export
-PMfixMatrix <- function(...){
+PMfixMatrix <- function(...) {
   cat("\nPMfixMatrix is deprecated.  Use PMcheck(...,fix=T) instead to fix data files.\n")
 }
 
 #' @export
-NPload <- function(...){
+NPload <- function(...) {
   cat("\nNPload is deprecated.  Use PMload() instead to load results of NPAG or IT2B runs.\n")
 }
 
 #' @export
-ITload <- function(...){
+ITload <- function(...) {
   cat("\nITload is deprecated.  Use PMload() instead to load results of NPAG or IT2B runs.\n")
 }
 
 #' @export
-NPreport <- function(...){
+NPreport <- function(...) {
   cat("\nNPreport is deprecated.  Use PMreport() instead to process completed NPAG or IT2B runs.\n")
 }
 
 #' @export
-ITreport <- function(...){
+ITreport <- function(...) {
   cat("\nITreport is deprecated.  Use PMreport() instead to process completed NPAG or IT2B runs.\n")
 }
 
 #' @export
-PMdiag <- function(...){
+PMdiag <- function(...) {
   cat("\nPMdiag is deprecated.  Use makeNPDE() instead to internally validate a model.\n")
 }
 
 #' @export
-PMreadDefaults <- function(...){
+PMreadDefaults <- function(...) {
   cat("\nPMreadDefaults is deprecated.  Use getPMoptions() instead to read Pmetrics options.\n")
 }
 
 #' @export
-PMwriteDefaults <- function(...){
+PMwriteDefaults <- function(...) {
   cat("\nPMwriteDefaults is deprecated.  Use setPMoptions() instead to set Pmetrics options.\n")
 }
 
 #' @export
-makeNPDE <- function(...){
+makeNPDE <- function(...) {
   cat("\nmakeNPDE is deprecated.  Use $validate() on a PM_result object or makeValid() instead.\n")
 }
 
 #' @export
-PMupdate <- function(...){
+PMupdate <- function(...) {
   cat("\nPMupdate is deprecated. Please restart Rstudio and do not load Pmetrics.  Then use the command devtools::install_github(\"LAPKB/Pmetrics\")")
 }
-
