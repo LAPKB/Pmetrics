850a0136dcd8161da47e577dd87c32d3722f294d
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-01-31 11:19:18 -0500

    fixed open HTML report on other OSs

--- R/PMutilities.R
+++ R/PMutilities.R
@@ -170,6 +170,7 @@ rmnorm <- function(n, mean, sigma) {
   retval
 }
 
+openHTML <- function(x) pander::openFileInOS(x)
 
 # parse NP_RF file only for final cycle information; used for bootstrapping
 # indpts,ab,corden,nvar,nactve,iaddl,icyctot,par
