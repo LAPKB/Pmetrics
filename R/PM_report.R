21a298b59863bd4a41e6b77a54e1e634d46c4334
Markus Herberg Hovd <m.h.hovd@farmasi.uio.no>
2023-03-13 12:08:43 -0700

    Error handling for pandoc

--- R/PM_report.R
+++ R/PM_report.R
@@ -1,25 +1,25 @@
 #' Generates a report from a specified Rmd template
-#' 
+#'
 #' @title Generate a report
 #' @param PM_result A `PM_result` object obtained from \code{PM_load(x)}, where `x` is the run number.
 #' @param template The path to a template. If not specified, uses the default Pmetrics report template.
 #' Pmetrics will prompt the user to set this address the first time the \code{remote} argument is set to \code{TRUE}
-#' in \code{\link{NPrun}}. 
+#' in \code{\link{NPrun}}.
 #' @import pander
 #' @import DT
 #' @import patchwork
 #' @import rmarkdown
+#' @import pandoc
 #' @return Generates an HTML-report in the current working directory.
 #' @author Markus Hovd and Julian Otalvaro
 #' @seealso \code{\link{PM_load}}
 #' @export
 
 PM_report <- function(PM_result, template, outfile, show = TRUE) {
-  
   if (!is(PM_result, "PM_result")) {
     stop("This function expects a valid PM_result object from PM_load\n")
   }
-
+  
   if (missing(template)) {
     template = system.file("report/templates/default.Rmd", package = "Pmetrics")
   }
@@ -36,8 +36,24 @@ PM_report <- function(PM_result, template, outfile, show = TRUE) {
   
   cat("Generating report based on specified template...\n")
   
+  # Check if pandoc is exposed to Rstudio
+  if (!rmarkdown::pandoc_available()) {
+    # Check if pandoc is installed
+    if (!pandoc::pandoc_available()) {
+      pandoc::pandoc_install()
+    }
+    
+    # Set the correct environmental variable for use in shell
+    Sys.setenv(RSTUDIO_PANDOC = pandoc::pandoc_locate())
+    
+    if (!rmarkdown::pandoc_available()) {
+      stop("Unable to install pandoc, or expose it to Rmarkdown.")
+    }
+    
+  }
+  
   rmarkdown::render(
-    input = template, 
+    input = template,
     output_file = outfile,
     params = list(res = PM_result),
     clean = TRUE,
