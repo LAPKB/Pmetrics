ea59bfd7eaf566491042b1ac131ff5f8058cbb8b
Markus Herberg Hovd <m.h.hovd@farmasi.uio.no>
2023-03-13 10:50:07 -0700

    First draft of new report template
    Added option to generate reports with custom templates using PM_report

---
+++ R/PM_report.R
@@ -0,0 +1,32 @@
+#' Generates a report from a specified Rmd template
+#' 
+#' @title Generate a report
+#' @param PM_result A `PM_result` object obtained from \code{PM_load(x)}, where `x` is the run number.
+#' @param template The path to a template. If not specified, uses the default Pmetrics report template.
+#' Pmetrics will prompt the user to set this address the first time the \code{remote} argument is set to \code{TRUE}
+#' in \code{\link{NPrun}}. 
+#' @import pander
+#' @import DT
+#' @import patchwork
+#' @return Generates an HTML-report in the current working directory.
+#' @author Markus Hovd and Julian Otalvaro
+#' @seealso \code{\link{PM_load}}
+#' @export
+
+PM_report <- function(PM_result, template) {
+  
+  if (!is(PM_result, "PM_result")) {
+    stop("This function expects a valid PM_result object from PM_load\n")
+  }
+
+  if (missing(template)) {
+    template = system.file("report/templates/default.Rmd", package = "Pmetrics")
+  }
+  
+  rmarkdown::render(
+    input = template, 
+    params = list(res = PM_result),
+    clean = TRUE,
+    quiet = TRUE
+  )
+}
