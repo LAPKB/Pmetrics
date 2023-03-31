2255c492725e2f9c54a8e67b0add40f832ec9375
Markus Herberg Hovd <m.h.hovd@farmasi.uio.no>
2023-03-31 14:37:40 +0200

    PM_report works with template ggplot_rust

--- R/PM_report.R
+++ R/PM_report.R
@@ -23,7 +23,8 @@ PM_report <- function(PM_result, template = getPMoptions("report_template"), out
   
   templateFile <- switch(template, 
                          plotly = system.file("report/templates/plotly.Rmd", package = "Pmetrics"),
-                         ggplot = system.file("report/templates/ggplot.Rmd", package = "Pmetrics"))
+                         ggplot = system.file("report/templates/ggplot.Rmd", package = "Pmetrics"),
+                         ggplot_rust = system.file("report/templates/ggplot_rust.Rmd", package = "Pmetrics"))
   
   if(is.null(templateFile)){
     if(!file.exists(templateFile)) stop(crayon::red("ERROR: "), templateFile, " does not exist.\n")
