abde98634b1b8e2e88cc855a3d2c0b7b90dd1767
Julian D. Otalvaro <juliandavid347@gmail.com>
2023-04-18 15:41:12 -0500

    hotfix auc

--- R/makeAUC.R
+++ R/makeAUC.R
@@ -1,12 +1,12 @@
 #' Calculates AUC from a variety of inputs
 #'
 #' Calculates the area under the time concentration curve using the
-#' trapezoidal approximation from a variety of inputs.  
-#' If a PM_pop, PM_post, PM_op, or PMsim object is specified, 
+#' trapezoidal approximation from a variety of inputs.
+#' If a PM_pop, PM_post, PM_op, or PMsim object is specified,
 #' `formula` is not required.
 #'
 #' @title Calculation of AUCs
-#' @param formula A formula of the form `obs ~ time`.  
+#' @param formula A formula of the form `obs ~ time`.
 #' This is only required with data that is not of class PM_pop, PM_post, PM_op or PM_sim.
 #' @param data A suitable data object, i.e. [PM_pop], [PM_post], [PM_op],
 #' [PM_sim], or some other suitable dataframe
@@ -32,7 +32,9 @@
 #' @seealso [PM_result], [PM_sim], [PM_op]
 #' @examples
 #' NPex$op$auc()
-#' Indometh %>% mutate(id = Subject) %>% makeAUC(conc ~ time)
+#' Indometh %>%
+#'   mutate(id = Subject) %>%
+#'   makeAUC(conc ~ time)
 #' @export
 
 makeAUC <- function(data,
@@ -42,102 +44,108 @@ makeAUC <- function(data,
                     icen = "median",
                     outeq = 1, block = 1,
                     method = "linear",
-                    addZero = F){
-  
+                    addZero = F) {
+  # handle objects
+  if (missing(data)) stop("Please supply a data object.\n")
 
-  #handle objects
-  if(missing(data)) stop("Please supply a data object.\n")
-  
   data_class <- which(
-    inherits(data, c("PMsim", "PM_sim", 
-                     "PMop", "PM_op", 
-                     "PMpop", "PM_pop",
-                     "PMpost", "PM_post"), which = T) == 1
-  ) #will be all zeros except matching class, undefined if none
-  
-  if(length(data_class)>0){ #there was a match
+    inherits(data, c(
+      "PMsim", "PM_sim",
+      "PMop", "PM_op",
+      "PMpop", "PM_pop",
+      "PMpost", "PM_post"
+    ), which = T) > 0
+  ) # will be all zeros except matching class, undefined if none
+
+  if (length(data_class) > 0) { # there was a match
     data2 <- switch(data_class,
-                    data$obs, #PMsim
-                    data$obs, #PM_sim
-                    data %>% mutate(out = obs), #PMop
-                    data$data %>% mutate(out = obs), #PM_op
-                    data %>% mutate(out = pred), #PMpop
-                    data$data %>% mutate(out = pred), #PM_pop
-                    data %>% mutate(out = pred), #PMpost
-                    data$data %>% mutate(out = pred), #PM_post
-                    )
-  } else { #class not matched, so needs formula
+      data$obs, # PMsim
+      data$obs, # PM_sim
+      data %>% mutate(out = obs), # PMop
+      data$data %>% mutate(out = obs), # PM_op
+      data %>% mutate(out = pred), # PMpop
+      data$data %>% mutate(out = pred), # PM_pop
+      data %>% mutate(out = pred), # PMpost
+      data$data %>% mutate(out = pred), # PM_post
+    )
+  } else { # class not matched, so needs formula
     if (missing(formula)) stop("\nPlease supply a formula of form 'out~time' for objects other than class PMsim, PMpost or PMop.")
-    y.name <- as.character(attr(terms(formula),"variables")[2])
-    x.name <- as.character(attr(terms(formula),"variables")[3])
-    if (length(grep(y.name,names(data)))==0) {stop(paste("\n'",y.name,"' is not a variable in the data.",sep=""))}
-    if (length(grep(x.name,names(data)))==0) {stop(paste("\n'",x.name,"' is not a variable in the data.",sep=""))}
+    y.name <- as.character(attr(terms(formula), "variables")[2])
+    x.name <- as.character(attr(terms(formula), "variables")[3])
+    if (length(grep(y.name, names(data))) == 0) {
+      stop(paste("\n'", y.name, "' is not a variable in the data.", sep = ""))
+    }
+    if (length(grep(x.name, names(data))) == 0) {
+      stop(paste("\n'", x.name, "' is not a variable in the data.", sep = ""))
+    }
     data2 <- data %>% dplyr::mutate(time = get(x.name), out = get(y.name))
   }
 
-  
-  #create dummy variables if missing
-  if(!"id" %in% names(data2)) data2$id <- 1 #add id if missing
-  if(!"outeq" %in% names(data2)) data2$outeq <- 1 #add outeq if missing
-  if(!"block" %in% names(data2)) data2$block <- 1 #add block if missing
-  if(!"icen" %in% names(data2)) data2$icen <- "median" #add icen if missing
-  if(missing(include)) include <- unique(data2$id)
-  if(missing(exclude)) exclude <- NA
 
-  #filter to create object to pass to auc calculation
+  # create dummy variables if missing
+  if (!"id" %in% names(data2)) data2$id <- 1 # add id if missing
+  if (!"outeq" %in% names(data2)) data2$outeq <- 1 # add outeq if missing
+  if (!"block" %in% names(data2)) data2$block <- 1 # add block if missing
+  if (!"icen" %in% names(data2)) data2$icen <- "median" # add icen if missing
+  if (missing(include)) include <- unique(data2$id)
+  if (missing(exclude)) exclude <- NA
+
+  # filter to create object to pass to auc calculation
   data3 <- data2 %>%
-    dplyr::filter(outeq == !!outeq,
-           block == !!block,
-           id %in% include,
-           !id %in% exclude,
-           time>=start & time<=end,
-           icen == !!icen,
-           !is.na(out)
+    dplyr::filter(
+      outeq == !!outeq,
+      block == !!block,
+      id %in% include,
+      !id %in% exclude,
+      time >= start & time <= end,
+      icen == !!icen,
+      !is.na(out)
     ) %>%
-    dplyr::select(id,time,out) %>%
+    dplyr::select(id, time, out) %>%
     dplyr::group_by(id)
-  
-  #auc function
+
+  # auc function
   get_auc <- function(df, addZero, method) {
-    if (addZero & !any(df$time == 0)) df <- rbind(data.frame(time=0, out=0), df)
+    if (addZero & !any(df$time == 0)) df <- rbind(data.frame(time = 0, out = 0), df)
     N <- nrow(df)
-    if(N <= 1) return(data.frame(NA, NA))
-    #(t_i - t_i-1)
+    if (N <= 1) {
+      return(data.frame(NA, NA))
+    }
+    # (t_i - t_i-1)
     diffTimes <- diff(df$time)
-    #(C_i + C_i-1)
+    # (C_i + C_i-1)
     sumConc <- df$out[-1] + df$out[-N]
-    #(C_i - C_i-1)
+    # (C_i - C_i-1)
     diffConc <- diff(df$out)
-    #log(C_i/C_i-1)
-    logConc <- log(df$out[-1]/df$out[-N])
-    #tmax
-    tmax <- which(df$out==max(df$out))
-    
-    #auc
-    if(method=="linear"){
+    # log(C_i/C_i-1)
+    logConc <- log(df$out[-1] / df$out[-N])
+    # tmax
+    tmax <- which(df$out == max(df$out))
+
+    # auc
+    if (method == "linear") {
       auc <- 0.5 * sum(diffTimes * sumConc)
     } else {
       auc <- sum(
-        #linear up
-        0.5 * sum ( diffTimes[1:tmax] * sumConc[1:tmax]),
-        #log down
-        sum(diffTimes[(tmax+1):(N-1)] * diffConc[(tmax+1):(N-1)]/logConc[(tmax+1):(N-1)])
+        # linear up
+        0.5 * sum(diffTimes[1:tmax] * sumConc[1:tmax]),
+        # log down
+        sum(diffTimes[(tmax + 1):(N - 1)] * diffConc[(tmax + 1):(N - 1)] / logConc[(tmax + 1):(N - 1)])
       )
     }
-    
+
     return(auc)
   }
-  
-  #calculate AUC
-  AUCdf <- tidyr::nest(data3, pk = !id) %>% 
+
+  # calculate AUC
+  AUCdf <- tidyr::nest(data3, pk = !id) %>%
     dplyr::mutate(tau = sapply(pk, get_auc, addZero, method)) %>%
     dplyr::select(id, tau)
-  
-  class(AUCdf) <- c("PMauc",class(AUCdf))
-  
-  #reorder according to original
-  AUCdf <- AUCdf[match(unique(data3$id),AUCdf$id),]
-  
+
+  class(AUCdf) <- c("PMauc", class(AUCdf))
+
+  # reorder according to original
+  AUCdf <- AUCdf[match(unique(data3$id), AUCdf$id), ]
+
   return(AUCdf)
 }
-
