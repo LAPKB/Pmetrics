2bb02b716fb5c4d7579cea3a9b253a461351e425
Michael Neely <mneely@usc.edu>
2023-05-22 17:01:56 -0700

    update covariates

--- R/build_model.R
+++ R/build_model.R
@@ -2,9 +2,10 @@
 #' @export
 #' 
 build_model <- function(data, ...) {
-  if(missing(data) || !inherits(data,"PM_data")) stop("Please specify a PM_data object.\n")
+  #if(missing(data) || !inherits(data,"PM_data")) stop("Please specify a PM_data object.\n")
+  if(missing(data)) data <- NULL
   #file_path <- system.file("Shiny/ModelBuilder/app.R", package = "Pmetrics")
-  file_path <- "../inst/Shiny/ModelBuilder/app.R" #comment out this when installed
+  file_path <- "inst/Shiny/ModelBuilder/app.R" #comment out this when installed
   
   if (!nzchar(file_path)) stop("Shiny app not found")
   ui <- server <- NULL # avoid NOTE about undefined globals
