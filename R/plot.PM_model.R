4fbab3d36db6e0c8c9d7fd5f76d3816c78099a54
Michael Neely <mneely@usc.edu>
2023-06-06 13:03:00 -0700

    Update documentation

--- R/plot.PM_model.R
+++ R/plot.PM_model.R
@@ -3,11 +3,47 @@
 #' Plots a [PM_model] based on differential equations using network plots from tidygraph and ggraph packages.
 #'
 #' @details
-#' This accepts a [PM_model] object with minimum of `$eqn` field and optionally an `$out` field within the
-#' `$model_list` field.
-#'
+#' This accepts a [PM_model] object and creates a network plot where nodes are compartments
+#' and edges are arrows connecting compartments.
 #' @method plot PM_model
 #' @param model The name of an [PM_model] object.
+#' @param marker Controls the characteristics of the compartments (nodes). 
+#' It can be boolean or a list.
+#' `TRUE` will plot the compartments with default characteristics.
+#' `FALSE` will suppress compartment plotting.
+#' If a list, can control some marker characteristics, including overriding defaults.
+#' These include:
+#' \itemize{
+#' \item{`color`} Marker color (default: dodgerblue).
+#' \item{`opacity`} Ranging between 0 (fully transparent) to 1 (fully opaque). Default is 0.5.
+#' \item{`size`} Relative size of boxes, ranging from 0 to 1.  Default is 0.25.
+#' \item{`line`} A list of  additional attributes governing the outline for filled shapes, most commonly
+#' color (default: black) and width (default: 0.5).
+#' }
+#' <br>
+#' <br>
+#' Example: `marker = list(color = "red", opacity = 0.8, line = list(color = "black", width = 1))`
+#' @param line Controls characteristics of arrows (edges).
+#' `TRUE` will plot default lines. `FALSE` will suppress lines.
+#' If a list, can control some line characteristics, including overriding defaults.
+#' These include: 
+#' \itemize{
+#' \item{`color`} Line color (default: black)
+#' \item{`width`} Thickness in points (default: 1).
+#' } 
+#' <br> 
+#' <br>
+#' Example: `line = list(color = "red", width = 2)`
+#' @param explicit A data frame or tibble containing two columns named `from` and `to` 
+#' to add additional connecting arrows to the plot indicating transfer between 
+#' compartments. For each row, the `from` column contains the compartment number of the arrow origin, and the 
+#' `to` column contains the compartment number of the arrow destination. Use 0 to indicate
+#' a destination to the external sink. e.g., `explicit = data.frame(from = 3, to = 0)`
+#' @param implicit Similar to `explicit`, used to add dashed connecting arrows 
+#' to the plot indicating implicit transfer between 
+#' compartments. For each row, the `from` column contains the compartment number of the arrow origin, and the 
+#' `to` column contains the compartment number of the arrow destination. Use 0 to indicate
+#' a destination to the external sink. e.g., `implicit = data.frame(from = 2, to = 4)`
 #'
 #' @return Plots the object.
 #' @author Markus Hovd, Julian Otalvaro, Michael Neely
@@ -17,9 +53,46 @@
 #' #to be implemented: NPex$model$plot()
 #' @family PMplots
 
-plot.PM_model <- function(model) {
+plot.PM_model <- function(model, marker = T, line = T, explicit, implicit) {
   
-  #TO DO: add customizations
+  marker <- if(marker != FALSE){
+    amendMarker(marker, default = list(color = "dodgerblue", size = 0.25, line = list(width = 0.5)))
+  } else {FALSE}
+  line <- if(line != FALSE){
+    amendLine(line, default = list(color = "black"))
+  } else {FALSE}
+  
+  #Add equations for algebraic models
+  if(is.null(model$model_list$eqn)){
+    key_vars <- c("ke", "v", "ka", "kcp", "kpc")
+    pri <- names(model$model_list$pri)
+    found_pri_keys <- key_vars %in% tolower(pri)
+    
+    if(!is.null(model$model_list$sec)){
+      found_sec_keys <- purrr::map_lgl(key_vars, \(x) stringr::str_detect(model$model_list$sec,
+                                                                          stringr::regex(x, ignore_case = TRUE)))
+    } else {found_sec_keys <- rep(NA, 5)}
+    found_keys <- key_vars[found_pri_keys | found_sec_keys] %>% na.exclude()
+    model$model_list$eqn <- dplyr::case_when(
+      all(found_keys %in% c("ke","v")) ~ c("dX[1] = RATEIV[1] - Ke*X[1]", 
+                                           NA, 
+                                           NA),
+      
+      all(found_keys %in% c("ke","v", "ka")) ~ c("dX[1] = BOLUS[1] - Ka*X[1]", 
+                                                 "dX[2] = RATEIV[1] + Ka*X[1] - Ke*X[2]",
+                                                 NA),
+      
+      all(found_keys %in% c("ke","v", "kcp", "kpc")) ~ c("dX[1] = RATEIV[1] - (Ke+KCP)*X[1] + KPC*X[2]", 
+                                                         "dX[2] = KCP*X[1] - KPC*X[2]",
+                                                         NA),
+      
+      all(found_keys %in% c("ke","v", "ka","kcp", "kpc")) ~ c("dX[1] = BOLUS[1] - Ka*X[1]", 
+                                                              "dX[2] = RATEIV[1] + Ka*X[1] - (Ke+KCP)*X[2] + KPC*X[3]",
+                                                              "dX[3] = KCP*X[2] - KPC*X[3]"),
+      .size = 3
+      
+    ) %>% na.exclude()
+  }
   
   #filter any equations that are not diffeq and make everything capital
   this_model <- model$model_list$eqn %>% 
@@ -207,10 +280,13 @@ plot.PM_model <- function(model) {
     dplyr::ungroup() %>%
     dplyr::mutate(from = ifelse(from == "", to, from)) %>%
     dplyr::mutate(to = ifelse(from == to, "", to)) %>%
-    dplyr::mutate(to = ifelse(to == "", as.numeric(max(to)) + 1, to)) %>%
-    dplyr::mutate(to = ifelse(is.na(to), as.numeric(max(from)) + 1, to)) %>%
+    dplyr::mutate(to = ifelse(to == "", as.numeric(max(c(to, from), na.rm = T)) + 1, to)) %>%
+    dplyr::mutate(to = ifelse(is.na(to), as.numeric(max(c(to, from), na.rm = T)) + 1, to)) %>%
     dplyr::filter(arrow != "") %>%
-    dplyr::mutate(across(everything(), as.character))
+    dplyr::mutate(across(everything(), as.character)) %>%
+    dplyr::mutate(node = stringr::str_extract(node, "\\d+")) %>%
+    dplyr::distinct(node, from, to) %>%
+    dplyr::mutate(implicit = FALSE)
   
   #outputs
   if(!is.null(purrr::pluck(model,"model_list","out",1,"val"))){
@@ -220,6 +296,36 @@ plot.PM_model <- function(model) {
     output_cmt = tibble::tibble(out = "", cmt = "1")
   }
   
+  #add explicit arrows from user
+  if(!missing(explicit)){
+    
+    max_to <- max(as.numeric(layout$to))
+    
+    if(!all(names(explicit) %in% c("from", "to"))){ stop("explicit should be a data frame with names from and to")}
+    imp <- explicit %>% 
+      dplyr::mutate(to = ifelse(to == 0, max_to, to)) %>%
+      dplyr::mutate(node = from, implicit = FALSE) %>%
+      dplyr::relocate(node, from, to, implicit) %>%
+      dplyr::mutate(across(c(node, from, to), as.character)) 
+    
+    layout <- dplyr::bind_rows(layout, imp)
+  }
+  
+  #add implicit arrows from user
+  if(!missing(implicit)){
+    
+    max_to <- max(as.numeric(layout$to))
+    
+    if(!all(names(implicit) %in% c("from", "to"))){ stop("implicit should be a data frame with names from and to")}
+    imp <- implicit %>% 
+      dplyr::mutate(to = ifelse(to == 0, max_to, to)) %>%
+      dplyr::mutate(node = from, implicit = TRUE) %>%
+      dplyr::relocate(node, from, to,implicit) %>%
+      dplyr::mutate(across(c(node, from, to), as.character)) 
+    
+    layout <- dplyr::bind_rows(layout, imp)
+  }
+  
   
   graph <- tidygraph::as_tbl_graph(layout) %>%
     dplyr::mutate(cmt = c(unique(layout$from),0)) %>%
@@ -227,31 +333,40 @@ plot.PM_model <- function(model) {
     dplyr::left_join(input_cmt, by = "cmt") %>%
     dplyr::mutate(input = ifelse(is.na(input),"",input)) %>%
     dplyr::left_join(output_cmt, by = "cmt") %>%
-    dplyr::mutate(out = ifelse(is.na(out),"",out))
+    dplyr::mutate(out = ifelse(is.na(out),"",out)) 
   
   
   
   
-  
-  g <- ggraph::ggraph(graph, layout = "tree") +
-    ggraph::geom_node_tile(aes(fill = position, linetype = position),
-                           width = .25, height = .25, alpha = 0.5) +
-    ggraph::geom_node_text(aes(label = input), nudge_x = .07, nudge_y = .05, color = "white") +
-    ggraph::geom_node_text(aes(label = out), nudge_x = -.07, nudge_y = .05, color = "black") +
-    
-    ggplot2::scale_fill_manual(values = c("dodgerblue", "grey80")) +
-    ggraph::geom_edge_fan(
-      arrow = grid::arrow(angle = 15, type = "closed",
-                          length = unit(6, 'mm')),
-      end_cap = ggraph::circle(3, 'mm'),
-      start_cap = ggraph::circle(4, 'mm'),
-      angle_calc = "across",
-      label_push = unit(-4, 'mm'),
-      edge_width = 1) +
+  g <- ggraph::ggraph(graph, layout = "tree")
+  if(!is.logical(marker)){ #will only be logical if FALSE
+    g <- g +
+      ggraph::geom_node_tile(aes(fill = position, linetype = position), 
+                             color = marker$line$color,
+                             lwd = marker$line$width,
+                             width = marker$size, height = marker$size, alpha = marker$opacity) +
+      ggraph::geom_node_text(aes(label = input), nudge_x = .07, nudge_y = .05, color = "white") +
+      ggraph::geom_node_text(aes(label = out), nudge_x = -.07, nudge_y = .05, color = "black") +
+      ggplot2::scale_fill_manual(values = c(marker$color, "grey80"))
+  }
+  if(!is.logical(line)){ #will only be logical if FALSE
+    g <- g +
+      ggraph::geom_edge_fan(
+        aes(linetype = as.numeric(implicit)+1),
+        arrow = grid::arrow(angle = 15, type = "closed",
+                            length = unit(6, 'mm')),
+        end_cap = ggraph::circle(3, 'mm'),
+        start_cap = ggraph::circle(4, 'mm'),
+        angle_calc = "across",
+        edge_color = line$color,
+        label_push = unit(-4, 'mm'),
+        edge_width = line$width) 
+  }
+  g <- g +
     ggraph::geom_node_label(aes(label = cmt), position = "identity") +
     ggraph::theme_graph() +
     ggplot2::theme(legend.position = "none")
   print(g)
-  return(graph)
+  return(invisible(graph))
 }
 
