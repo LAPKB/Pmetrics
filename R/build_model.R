50867116a5e64977265d1869fa80956c64e84a6b
Michael Neely <mneely@usc.edu>
2023-06-07 10:55:24 -0700

    documentation

--- R/build_model.R
+++ R/build_model.R
@@ -1,6 +1,9 @@
 #' Launch Model Builder app
 #' 
-#' Opens the shiny model builder app.
+#' Open the shiny model builder app.
+#' 
+#' @details
+#' The app will open in a separate window.
 #'
 #' @param data Optional [PM_data] object which will be used to read
 #' covariates.
@@ -8,27 +11,19 @@
 #' @return Launches the shiny app.
 #' @export
 #' @author Michael Neely
-#' @examplesIf
-#' ${1:interactive()}
-#' ${1:build_model(NPex$data)} 
 #' 
 build_model <- function(data, ...) {
-  #if(missing(data) || !inherits(data,"PM_data")) stop("Please specify a PM_data object.\n")
   if(missing(data)) data <- NULL
   file_path <- system.file("Shiny/ModelBuilder/app.R", package = "Pmetrics")
   #file_path <- "inst/Shiny/ModelBuilder/app.R" #comment out this when installed
   
   if (!nzchar(file_path)) stop("Shiny app not found")
   ui <- server <- NULL # avoid NOTE about undefined globals
-  #source("Utils.R", local = TRUE)
   source(file_path, local = TRUE)
   server_env <- environment(server)
-  
-  # Here you add any variables that your server can find
   server_env$data <- data
   
   app <- shiny::shinyApp(ui, server)
   shiny::runApp(app, ...)
-  #shiny::runGadget(app, viewer = dialogViewer("Pmetrics Model Builder", width = 1200, height = 1200))
 }
 
