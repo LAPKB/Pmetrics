998a97cadafdca4b6a228aa5a7928dddf3b01805
Michael Neely <mneely@usc.edu>
2023-07-04 19:56:55 +0100

    pta vignette start

--- R/DataDescriptions.R
+++ R/DataDescriptions.R
@@ -207,11 +207,19 @@
 #' depo compartment(s).
 #' * **par** Either "K" for parameterization with rate constants, or "CL" for
 #' clearances.
-#' * **bolus** Does the model contain a bolus input, e.g. oral?
+#' * **route** Input route(s)
 #' * **elim** List of compartment numbers with elimination
-#' * **ode** Does the model contain differential equations?
 #' * **mod** the [PM_model]
 #' * **name** Model name in plain English
+#' * **algebraic** Token indicating algebraic solution. Format is a character
+#' vector of 4 elements:
+#'   - P[]: primary parameters of at least Ke, V, and optionally Ka, KCP and KPC,
+#'   e.g. P[Ke,V]
+#'   - B[]: Bolus input compartment numbers, 0 for no bolus, e.g. B[1]
+#'   - R[]: RateIV inputs, 0 for no IV, e.g. R[2]
+#'   - O[]: Output compartments, e.g. O[2]
+#' For example, one compartment IV model with no oral input: 
+#' "P[Ke,V], B[0], R[1], O[1]"
 #' @author Michael Neely
 #' @keywords datasets
 #'
