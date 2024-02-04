0d825231cb4059c781172493ac28cc6f3130fde1
Markus Hovd <66058642+mhovd@users.noreply.github.com>
2024-02-04 15:52:55 +0100

    Update brew install script location
    Brew has changed the name of the main branch from `master` to `HEAD`, as such, Brew will not install correctly on new installations of Pmetrics.

--- inst/mac/install_homebrew.sh
+++ inst/mac/install_homebrew.sh
@@ -1,1 +1,1 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL  https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
