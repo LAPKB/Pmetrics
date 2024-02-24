cfc53aa18b6c4e911b0254f5916ef40d5d2756d7
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-02-24 15:19:19 +0000

    PM_fit example

--- data-raw/PMex.R
+++ data-raw/PMex.R
@@ -1,47 +1,49 @@
-#code to prepare example datasets for Pmetrics
-#re-run before building package when datasets change
-#edit DataDescriptions.R for documentation of datasets
+# code to prepare example datasets for Pmetrics
+# re-run before building package when datasets change
+# edit DataDescriptions.R for documentation of datasets
 
 library(Pmetrics)
 library(usethis)
 
 setwd("~/LAPK/Development/Pmetrics/data-raw")
 
-#model file
+# model file
 modEx <- PM_model$new("model.txt")
 usethis::use_data(modEx, overwrite = T)
 
-#data
+# data
 dataEx <- PM_data$new("ex.csv")
 usethis::use_data(dataEx, overwrite = T)
 
-#bad data
-badData<- PM_data$new("bad.csv")
+# fit object
+fitEx <- PM_fit$new(model = modEx, data = dataEx)
+usethis::use_data(dataEx, overwrite = T)
+
+# bad data
+badData <- PM_data$new("bad.csv")
 usethis::use_data(badData, overwrite = T)
 
 
 setwd("Runs")
-#NPAG
+# NPAG
 NPex <- PM_load(1)
 usethis::use_data(NPex, overwrite = T)
 
-#IT2B
+# IT2B
 ITex <- PM_load(2)
 usethis::use_data(ITex, overwrite = T)
 
 setwd("..")
 
-#simulator example
+# simulator example
 simEx <- NPex$sim(
   limits = c(0, 3), data = "ptaex1.csv",
   predInt = c(120, 144, 0.5), seed = rep(-17, 4)
 )
 usethis::use_data(simEx, overwrite = T)
 
-#NPex with valid field
+# NPex with valid field
 setwd("Valid")
 NPex$validate()
 NPex_val <- NPex
 usethis::use_data(NPex_val, overwrite = T)
-
-
