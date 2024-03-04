ff90b0a6fbc98c2dbb04471f7c9225d4f637a2e8
Michael Neely <mneely@usc.edu>
2024-03-05 08:14:24 +1000

    update imports

--- R/Pmetrics-package.R
+++ R/Pmetrics-package.R
@@ -21,7 +21,7 @@
 #' @importFrom ggplot2 ggplot aes geom_line geom_point geom_polygon geom_hline
 #' scale_x_log10 scale_x_continuous scale_y_log10 scale_y_continuous xlab ylab
 #' theme ggtitle element_blank geom_segment aes_string aes_string theme_bw theme_grey
-#' @importFrom purrr map map2 reduce map_chr keep pluck
+#' @importFrom purrr map map2 reduce map_chr keep pluck map_lgl
 #' @importFrom magrittr %>%
 # #' @importFrom tibble as_tibble tibble
 #' @importFrom tidyr pivot_longer pivot_wider nest unnest extract separate fill crossing
@@ -45,7 +45,7 @@
 # #' @importFrom base64enc base64decode
 #' @importFrom R6 R6Class
 #' @importFrom plotly filter mutate plot_ly add_markers add_lines layout 
-#' ggplotly subplot add_annotations add_bars
+#' ggplotly subplot add_annotations add_bars renderPlotly
 #' @importFrom shiny fluidPage titlePanel tags navlistPanel tabPanel fluidRow
 #' column h2 h3 h4 h5 fileInput selectInput numericInput radioButtons checkboxInput
 #' textInput uiOutput actionButton hr textAreaInput reactiveVal reactive
