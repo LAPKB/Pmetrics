d7b88417cfb543e7f763a8c545debbaf49c82172
Michael Neely <mneely@usc.edu>
2024-04-23 14:37:31 -0700

    Add archived folder to github, ignored in Rbuild

---
+++ Archived/data-raw/PMex.R
@@ -0,0 +1,50 @@
+# code to prepare example datasets for Pmetrics
+# re-run before building package when datasets change
+# edit DataDescriptions.R for documentation of datasets
+
+library(Pmetrics)
+library(usethis)
+
+setwd("~/LAPK/Development/Pmetrics/data-raw")
+
+# model file
+modEx <- PM_model$new("model.txt")
+usethis::use_data(modEx, overwrite = T)
+
+# data
+dataEx <- PM_data$new("ex.csv")
+usethis::use_data(dataEx, overwrite = T)
+
+# fit object
+fitEx <- PM_fit$new(model = modEx, data = dataEx)
+usethis::use_data(dataEx, overwrite = T)
+
+# bad data
+badData <- PM_data$new("bad.csv")
+usethis::use_data(badData, overwrite = T)
+
+
+setwd("Runs")
+# NPAG
+NPex <- PM_load(1)
+usethis::use_data(NPex, overwrite = T)
+
+# IT2B
+ITex <- PM_load(2)
+usethis::use_data(ITex, overwrite = T)
+
+setwd("..")
+
+# simulator example
+simEx <- NPex$sim(
+  limits = c(0, 3), data = "ptaex1.csv",
+  predInt = c(120, 144, 0.5), seed = rep(-17, 4)
+)
+usethis::use_data(simEx, overwrite = T)
+
+# NPex with valid field
+setwd("Valid")
+NPex_val <- NPex
+NPex_val$validate()
+
+usethis::use_data(NPex_val, overwrite = T)
---
+++ Archived/doc/intro.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, echo=F, message=F-------------------------------------------------
+library(Pmetrics)
+library(tidyverse)
+library(plotly)
+
