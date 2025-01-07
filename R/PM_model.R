97ff94227d5d2e61b497b1a35bb08f4e121d6f79
Michael Neely <mneely@usc.edu>
2025-01-06 23:41:31 -0800

    add model_lib()

--- R/PM_model.R
+++ R/PM_model.R
@@ -1859,6 +1859,125 @@ plot.PM_model <- function(x, marker = TRUE, line = TRUE, explicit, implicit, ...
   print(g)
   return(invisible(graph))
 }
+
+
+# MODEL LIBRARY -----------------------------------------------------------
+
+#' @title Pharmacokinetic model library
+#' @description
+#' `r lifecycle::badge("stable")`
+#'
+#' This function provides a list of available pharmacokinetic models.
+#' @param name The name of the model to display. If `NULL`, the entire list is displayed.
+#' @param show If `TRUE`, the model is displayed in the console. If `FALSE`, the model is only returned as a tibble.
+#' @return A tibble with the model equations.
+#' @author Michael Neely
+#' @seealso [PM_model]
+#' @export
+#' @examples
+#' model_lib() 
+#' model_lib("one_comp_iv")
+
+model_lib <- function(name = NULL, show = TRUE){
+  
+  
+  mod_table <- matrix(
+    c(
+      
+      "one_comp_iv", "advan1\nadvan1-trans1", "One compartment IV", "1 = Central", "Ke, V",
+      "one_comp_iv_cl", "advan1\nadvan1-trans2", "One compartment IV", "1 = Central", "CL, V",
+      
+      "two_comp_bolus", "advan2\nadvan2-trans1", "Two compartment bolus", "1 = Bolus\n2 = Central", "Ka, Ke, V",
+      "two_comp_bolus_cl", "advan2\nadvan2-trans2", "Two compartment bolus", "1 = Bolus\n2 = Central", "Ka, CL, V",
+      
+      "two_comp_iv", "advan3\nadvan3-trans1", "Two compartment IV", "1 = Central\n2 = Peripheral", "Ke, V, KCP, KPC",
+      "two_comp_iv_cl", "advan3\nadvan3-trans4", "Two compartment IV", "1 = Central\n2 = Peripheral", "CL, V1, Q, V2",
+      
+      "three_comp_bolus", "advan4\nadvan4-trans1", "Three compartment bolus", "1 = Bolus\n2 = Central\n3 = Peripheral", "Ka, Ke, V, KCP, KPC",
+      "three_comp_bolus_cl", "advan4\nadvan4-trans4", "Three compartment bolus", "1 = Bolus\n2 = Central\n3 = Peripheral", "Ka, CL, V2, Q, V3"
+    ),
+    ncol = 5, byrow = TRUE
+  ) %>%
+    as.data.frame() %>%
+    setNames(c("Primary Name", "Alt Names", "Description", "Compartments", "Parameters")) %>%
+    as_tibble()
+  
+  
+  mod_table$ODE <- list(
+    list(
+      "dX[1] = RATEIV[1] - Ke*X[1]"
+    ),
+    list(
+      "dX[1] = RATEIV[1] - CL/V*X[1]"
+    ),
+    list(
+      "dX[1] = BOLUS[1] - Ka*X[1]",
+      "dX[2] = RATEIV[1] + Ka*X[1] - Ke*X[2]"
+    ),
+    list(
+      "dX[1] = BOLUS[1] - Ka*X[1]",
+      "dX[2] = RATEIV[1] + Ka*X[1] - CL/V*X[2]"
+    ),
+    list(
+      "dX[1] = RATEIV[1] - (Ke + KCP)*X[1] + KPC*X[2]",
+      "dX[2] = KCP*X[1] - KPC*X[2]"
+    ),
+    list(
+      "dX[1] = RATEIV[1] - (CL + Q)/V1*X[1] + Q/V2*X[2]",
+      "dX[2] = Q/V1*X[1] - Q/V2*X[2]"
+    ),
+    list(
+      "dX[1] = BOLUS[1] - Ka*X[1]",
+      "dX[2] = RATEIV[1] + Ka*X[1] - (Ke + KCP)*X[2] + KPC*X[3]",
+      "dX[3] = KCP*X[2] - KPC*X[3]"
+    ),
+    list(
+      "dX[1] = BOLUS[1] - Ka*X[1]",
+      "dX[2] = RATEIV[1] + Ka*X[1] - (CL + Q)/V2*X[2] + Q/V3*X[3]",
+      "dX[3] = Q/V2*X[2] - Q/V3*X[3]"
+    )
+  )
+  
+  
+  if(is.null(name)){
+    print(mod_table %>%
+            rowwise() %>%
+            mutate(ODE = paste(unlist(ODE), collapse = "\n")) %>% 
+            flextable() %>% 
+            set_header_labels(ODE = "Corresponding ODE") %>%
+            autofit())
+    return(invisible(NULL))
+  }
+  
+  if(! tolower(name) %in% 
+     c(
+       glue::glue("one_comp_iv{c('','_cl')}"),
+       glue::glue("two_comp_bolus{c('','_cl')}"),
+       glue::glue("two_comp_iv{c('','_cl')}"),
+       glue::glue("three_comp_bolus{c('','_cl')}"),
+       glue::glue("advan{1:4}"),
+       glue::glue("advan{1:4}-trans1"),
+       glue::glue("advan{1:2}-trans2"),
+       "advan3-trans4",
+       "advan4-trans4"
+     )
+     
+  ){
+    cli::cli_abort(c("x" = "Invalid model name"))
+  }
+  
+  if(show){
+    print(mod_table %>% filter(`Primary Name` == name) %>% 
+            mutate(ODE = paste(unlist(ODE), collapse = "\n")) %>% 
+            flextable() %>% 
+            set_header_labels(ODE = "Corresponding ODE") %>%
+            autofit())
+  }
+  
+  return(invisible(mod_table %>% filter(`Primary Name` == name) %>% select(ODE) %>% pluck(1,1) %>% unlist()  ))
+}
+
+
 # convert_r_to_rust <- function(r_code) {
 #   # Helper functions
 #   add_float_notation <- function(text) {
