171ac08214cdfa72cda84334861ce666717ccb7f
Julian D. Otalvaro <juliandavid347@gmail.com>
2025-04-02 18:32:25 +0100

    newer version of PMcore

--- src/rust/src/executor.rs
+++ src/rust/src/executor.rs
@@ -1,33 +1,14 @@
 use crate::settings::settings;
 use extendr_api::List;
-use libloading::{Library, Symbol};
-use logger::setup_log;
-use pmcore::prelude::*;
-use settings::{write_settings_to_file, Settings};
+
+use pmcore::prelude::{pharmsol::exa::load::load_ode, *};
+
 use std::path::PathBuf;
 
 use crate::simulation::SimulationRow;
 
-unsafe fn load_ode(lib: &Library) -> (ODE, Meta) {
-    let create_eqn: Symbol<unsafe extern "C" fn() -> *mut std::ffi::c_void> =
-        unsafe { lib.get(b"create_eqn_ptr").expect("Failed to load symbol") };
-    let eqn_ptr = unsafe { create_eqn() };
-
-    let create_meta: Symbol<unsafe extern "C" fn() -> *mut std::ffi::c_void> =
-        unsafe { lib.get(b"metadata_ptr").expect("Failed to load symbol") };
-    let meta_ptr = unsafe { create_meta() };
-
-    unsafe {
-        (
-            (&*(eqn_ptr as *mut equation::ODE)).clone(),
-            (&*(meta_ptr as *mut equation::Meta)).clone(),
-        )
-    }
-}
-
 pub(crate) fn model_parameters(model_path: PathBuf) -> Vec<String> {
-    let lib = unsafe { Library::new(model_path).expect("Failed to load library") };
-    let (_, meta) = unsafe { load_ode(&lib) };
+    let (_lib, (_ode, meta)) = unsafe { load_ode(model_path) };
     meta.get_params().clone()
 }
 
@@ -37,38 +18,26 @@ pub(crate) fn simulate(
     support_point: &Vec<f64>,
     spp_index: usize,
 ) -> Vec<SimulationRow> {
-    let lib = unsafe { Library::new(model_path).expect("Failed to load library") };
-    let (eq, meta) = unsafe { load_ode(&lib) };
+    let (_lib, (ode, meta)) = unsafe { load_ode(model_path) };
     assert!(meta.get_params().len() == support_point.len());
     SimulationRow::from_subject_predictions(
-        eq.estimate_predictions(subject, support_point),
+        ode.estimate_predictions(subject, support_point),
         subject.id(),
         spp_index,
     )
 }
 
-pub(crate) fn fit(model_path: PathBuf, data: PathBuf, params: List, output_path: PathBuf) {
-    let lib = unsafe { Library::new(model_path).expect("Failed to load library") };
-    let (eq, meta) = unsafe { load_ode(&lib) };
-    // dbg!(&meta);
-    let settings = settings(params, meta.get_params(), output_path.to_str().unwrap());
-    write_settings_to_file(&settings).unwrap();
-    initialize_logging(&settings);
+pub(crate) fn fit(
+    model_path: PathBuf,
+    data: PathBuf,
+    params: List,
+    output_path: PathBuf,
+) -> std::result::Result<(), anyhow::Error> {
+    let (_lib, (eq, meta)) = unsafe { load_ode(model_path) };
+    let settings = settings(params, meta.get_params(), output_path.to_str().unwrap())?;
     let data = data::read_pmetrics(data.to_str().unwrap()).expect("Failed to read data");
     let mut algorithm = dispatch_algorithm(settings, eq, data).unwrap();
     let result = algorithm.fit().unwrap();
     result.write_outputs().unwrap();
-}
-
-use std::sync::Once;
-
-static INIT: Once = Once::new();
-
-pub fn initialize_logging(settings: &Settings) {
-    INIT.call_once(|| {
-        if let Err(e) = setup_log(settings) {
-            eprintln!("Failed to initialize logging: {}", e);
-            // Handle the error as needed, possibly panic or log to stderr
-        }
-    });
+    Ok(())
 }
--- src/rust/src/settings.rs
+++ src/rust/src/settings.rs
@@ -1,22 +1,26 @@
 use extendr_api::List;
-use pmcore::prelude::settings::*;
+use pmcore::prelude::*;
 use std::collections::HashMap;
-use toml::Table;
 
-pub(crate) fn settings(ranges: List, params: &Vec<String>, output_path: &str) -> Settings {
+pub(crate) fn settings(ranges: List, params: &Vec<String>, output_path: &str) -> Result<Settings> {
     let ranges = robj_to_hashmap(ranges);
-    let mut settings = Settings::default();
-
-    settings.random = Random {
-        parameters: create_random_parameters(ranges, params),
-    };
-
-    settings.output = Output {
-        write: true,
-        path: output_path.to_string(),
-    };
-    settings.config.algorithm = "NPAG".to_string();
-    settings
+    let parameters = parse_parameters(ranges, params)?;
+    let mut settings = Settings::builder()
+        .set_algorithm(pmcore::prelude::Algorithm::NPAG)
+        .set_parameters(parameters)
+        .set_error_model(
+            pmcore::prelude::ErrorModel::Additive,
+            0.5,
+            (0.0, 0.5, 0.0, 0.0),
+        )
+        .build();
+    settings.set_cycles(1000);
+    settings.set_prior(Prior::sobol(2028, 22));
+    settings.set_output_path(output_path.to_string());
+    settings.set_write_logs(true);
+    settings.write()?;
+    settings.initialize_logs()?;
+    Ok(settings)
 }
 
 fn robj_to_hashmap(list: List) -> HashMap<String, (f64, f64)> {
@@ -30,20 +34,25 @@ fn robj_to_hashmap(list: List) -> HashMap<String, (f64, f64)> {
     map
 }
 
-fn create_random_parameters(ranges: HashMap<String, (f64, f64)>, params: &Vec<String>) -> Table {
-    let mut random_parameters = Table::new();
+fn parse_parameters(
+    ranges: HashMap<String, (f64, f64)>,
+    params: &Vec<String>,
+) -> Result<Parameters> {
+    let mut parameters = Parameters::new();
     for param in params.iter() {
         let (min, max) = match ranges.get(param) {
             Some(range) => range,
             None => {
-                eprintln!("Parameter {} not found in ranges {:?}", param, &ranges);
-                panic!()
+                // eprintln!("Parameter {} not found in ranges {:?}", param, &ranges);
+                return Err(anyhow::anyhow!(
+                    "Parameter {} not found in ranges {:?}",
+                    param,
+                    &ranges
+                ));
             }
         };
-        random_parameters.insert(
-            param.to_string(),
-            toml::Value::Array(vec![toml::Value::Float(*min), toml::Value::Float(*max)]),
-        );
+        parameters = parameters.add(param.to_string(), *min, *max, false);
     }
-    random_parameters
+
+    Ok(parameters)
 }
