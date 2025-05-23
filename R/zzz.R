6e8dd659bc4b53d7d16205f78dc7503584a6eafc
Markus <66058642+mhovd@users.noreply.github.com>
2025-05-23 11:07:17 +0200

    Remove tests, clean DESCRIPTION

--- R/zzz.R
+++ R/zzz.R
@@ -10,44 +10,6 @@
       "\nUse PMmanual() or visit the LAPK website at http://www.lapk.org/pmetrics.php for help.",
       "\n", crayon::green("Use PM_tutorial() for an introduction.")
     )
-
-    # response <- tryCatch(
-    #   httr::GET(
-    #     "https://api.github.com/repos/LAPKB/Pmetrics/releases/latest",
-    #     httr::add_headers(Accept = "application/vnd.github.v3+json")
-    #   ),
-    #   error = function(e) {
-    #     packageStartupMessage(e)
-    #     return(NULL)
-    #   }
-    # )
-    # currentVersion <- "0.1"
-    # if (!is.null(response)) {
-    #   if (response$status == 200) {
-    #     currentVersion <- package_version(gsub("v", "", httr::content(response)$tag_name))
-    #   }
-    # }
-
-    # if (currentVersion > installedVersion) {
-    #   packageStartupMessage(paste("\nPmetrics version ", currentVersion, " is available from www.lapk.org/Pmetrics_install.php. You have version ", installedVersion, ".\n", sep = ""))
-    # }
-
-    # messages <- suppressWarnings(
-    #   tryCatch(scan(file, skip = 2, sep = "\n", what = "character", quiet = T),
-    #     error = function(e) e <- "NoConnect"
-    #   )
-    # )
-    # if (length(messages) > 1) {
-    #   for (i in messages) {
-    #     temp <- strsplit(i, ">")
-    #     if (temp[[1]][1] == "all") {
-    #       msg <- c(msg, paste("\n", temp[[1]][2], "\n", sep = ""))
-    #     } else {
-    #       temp2 <- strsplit(temp[[1]][1], ",")
-    #       if (temp2[[1]][1] == installedVersion & temp2[[1]][2] == OS) msg <- c(msg, (paste("\n", temp[[1]][2], "\n", sep = "")))
-    #     }
-    #   }
-    # }
     packageStartupMessage(msg)
   }
 
