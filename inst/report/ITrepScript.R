a7d1e70cc7425ea5d3af3c469c774a875c2b1968
Michael Neely <mneely@usc.edu>
2025-11-12 15:46:02 -0800

    remove old files

--- inst/report/ITrepScript.R
+++
@@ -1,9 +0,0 @@
-require(Pmetrics)
-wd <- commandArgs()[6]
-icen <- commandArgs()[7]
-setwd(wd)
-unused <- Pmetrics:::makeRdata(wd, 2)
-res <- PM_load(file = "PMout.Rdata")
-Pmetrics:::PM_report(res, outfile = "IT2Breport.html")
-
-
