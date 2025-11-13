56772acefb9cf74cb3880a8a88f68843922da480
Michael Neely <mneely@usc.edu>
2025-11-13 15:39:43 -0800

    wip: update to PMcore 0.21.1

--- R/PM_model.R
+++ R/PM_model.R
@@ -1366,7 +1366,9 @@ PM_model <- R6::R6Class(
                   output_path <- tempfile(pattern = "model_", fileext = ".pmx")
                   cli::cli_inform(c("i" = "Compiling model..."))
                   tryCatch({
-                    compile_model(model_path , output_path, private$get_primary(), kind = tolower(self$model_list$type))
+                    compile_model(model_path , output_path, private$get_primary(), 
+                    template_path = system.file(package = "Pmetrics"), 
+                    kind = tolower(self$model_list$type))
                     self$binary_path <- output_path
                   }, error = function(e) {
                     cli::cli_abort(
