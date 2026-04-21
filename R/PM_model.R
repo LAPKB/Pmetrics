ca0e504d3e0bd036b55abcbc4da13ccfe149e62b
Michael Neely <mneely@usc.edu>
2026-04-21 10:11:13 -0700

    fix: remove stale ggraph dependency (#331)
    * fix: remove stale ggraph dependency
    
    * fix: remove stale ggraph reference

--- R/PM_model.R
+++ R/PM_model.R
@@ -2184,7 +2184,7 @@ interp <- function(type = "lm") {
 #' @description
 #' `r lifecycle::badge("stable")`
 #'
-#' Plots a [PM_model] based on differential equations using network plots from tidygraph and ggraph packages.
+#' Plots a [PM_model] based on differential equations using ggplot.
 #'
 #' @details
 #' This accepts a [PM_model] object and creates a network plot where nodes are compartments
@@ -2232,7 +2232,7 @@ interp <- function(type = "lm") {
 #' @param ... Not used.
 #' @return A plot object of the model.
 #' @author Markus Hovd, Julian Otalvaro, Michael Neely
-#' @seealso [PM_model], [ggraph::ggraph()], [ggplot2::ggplot()]
+#' @seealso [PM_model], [ggplot2::ggplot()]
 #' @export
 #' @examples
 #' \dontrun{
