6345e6789896a667e28ccaf15dba591ad75c8e08
Michael Neely <mneely@usc.edu>
2026-05-13 13:59:15 -0700

    fix: typos add apps packages as suggests

--- R/mod_lib.R
+++ R/mod_lib.R
@@ -147,7 +147,7 @@ model_lib <- function(show = TRUE, launch.browser = TRUE) {
     if (!requireNamespace("PmetricsModelLib", quietly = TRUE)) {
       cli::cli_abort(c(
         "x" = "The {.pkg PmetricsModelLib} package is required to launch model_lib.",
-        "i" = "Install it using install.packages('PmetricsModelLib', repos = 'https://lapkb.r-universe.dev'."
+        "i" = "Install it using install.packages('PmetricsModelLib', repos = 'https://lapkb.r-universe.dev')."
       ))
     }
     getExportedValue("PmetricsModelLib", "run_app")(launch.browser = launch.browser)
