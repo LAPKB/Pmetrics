6b50eee0d953780ddf2d4c900affeb5cce88abdb
Markus Hovd <markushh@uio.no>
2026-09-03 19:03:45 +0200

    fix: Regenerate stale example data (#381)
    Also adds a test in CI to see if the fields match current API; but does not check the actual value.

--- R/PM_data.R
+++ R/PM_data.R
@@ -1341,6 +1341,10 @@ writeErrorFile <- function(dat, err, legacy, wb, sheet) {
   row.names(errDF) <- 1:nrow(errDF)
   names(errDF) <- c("row", "column", "code")
   errors <- errDF[!is.na(errDF$row), ]
+  # errList mixes row numbers with column names (the non-numeric-column check),
+  # so errDF comes back all character; restore the indices that must be numeric.
+  errors$column <- suppressWarnings(as.numeric(errors$column))
+  errors$code <- as.numeric(errors$code)
   formattedCols <- names(dat)
 
   if (legacy) {
@@ -1375,7 +1379,9 @@ writeErrorFile <- function(dat, err, legacy, wb, sheet) {
   }
 
   # make second table to summarize errors
-  error_summary <- errors |> filter(!code %in% c(10, 13, 15)) # we will add these back
+  error_summary <- errors |>
+    filter(!code %in% c(10, 13, 15)) |> # we will add these back
+    mutate(row = as.numeric(row))
 
   # Highlight the cells with errors
   for (i in 1:nrow(errors)) {
@@ -1385,6 +1391,7 @@ writeErrorFile <- function(dat, err, legacy, wb, sheet) {
     # special highlighting - overwrite some values
     if (thisErr$code == 10) {
       # if covariate error
+      rowIndex <- as.numeric(rowIndex)
       covData <- getCov(dat)
       colIndex <- covData$covstart +
         which(is.na(dat[rowIndex, covData$covstart:covData$covend])) - 1
@@ -1456,7 +1463,7 @@ writeErrorFile <- function(dat, err, legacy, wb, sheet) {
       )
     } else {
       # add the highlighting and comments for other errors
-      rowIndex <- rowIndex + 1 + legacy_offset
+      rowIndex <- as.numeric(rowIndex) + 1 + legacy_offset
       comment <- openxlsx::createComment(errorsTable$comment[thisErr$code], author = "Pmetrics", visible = F)
       openxlsx::addStyle(wb, sheet, errStyle1, rowIndex, colIndex)
       openxlsx::writeComment(wb, sheet, xy = c(colIndex, rowIndex), comment = comment)
